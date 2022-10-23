667140ade97ea31a967dd74b7d5b800ec95e6021
Alessio De Nicolò <alessio1.denicolo@mail.polimi.it>
2022-10-23 16:46:23 +0200

    Sprint and crouch added
    The crouch in air has to be fixed

--- Assets/Code/Scripts/RealityMovement.cs
+++ Assets/Code/Scripts/RealityMovement.cs
@@ -1,28 +1,38 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
 
 public class RealityMovement : MonoBehaviour
 {
-    public float moveSpeed; // speed intensity
+    [Header("Speed")]
+    public float walkSpeed;
+    public float sprintSpeed;
+    
+    private float _moveSpeed;     // speed intensity
+    
+    [Header("Drag")]
+    public float groundDrag;    // ground drag
+    
+    [Header("Jump")]
+    public float jumpForce;     // set jump upward force
+    public float jumpCooldown;      // set jump cooldown
+    public float airMultiplier;     // set air movement limitation
+    private bool _readyToJump;      //
 
+    [Header("Crouch")]
+    public float crouchSpeed;
+    public float crouchYScale;
+    private float _startYScale;
+    
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;     // set the jump key
+    public KeyCode sprintKey = KeyCode.LeftShift;   // set the sprint key
+    public KeyCode crouchKey = KeyCode.LeftControl;
+
+    [Header("Ground Check")]
     // set the ground check
-    public float groundDrag;
     public float playerHeight;
     public LayerMask ground;
     private bool _grounded;
     
-    // set jump
-    public float jumpForce;
-    public float jumpCooldown;
-    public float airMultiplier;
-    private bool _readyToJump;
-    
-    // set the jump key
-    public KeyCode jumpKey = KeyCode.Space;
-
     public Transform orientation;
 
     private float _horizontalInput;
@@ -30,7 +40,17 @@ public class RealityMovement : MonoBehaviour
 
     private Vector3 _moveDirection;
 
-    private Rigidbody _rb;
+    private Rigidbody _rb;      // set the rigidbody
+
+    // player states
+    public MovementState state;     // current player state
+    public enum MovementState       // define player states
+    {
+        Walking,
+        Sprinting,
+        Crouching,
+        Air
+    }
 
 
     // Start is called before the first frame update
@@ -39,7 +59,9 @@ public class RealityMovement : MonoBehaviour
         _rb = GetComponent<Rigidbody>();
         _rb.freezeRotation = true;
         
-        ResetJump(); // set readyToJump to "true"
+        ResetJump(); // set _readyToJump to "true"
+
+        _startYScale = transform.localScale.y;
     }
 
     // Update is called once per frame
@@ -48,6 +70,7 @@ public class RealityMovement : MonoBehaviour
         _grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, ground);
 
         MyInput();
+        StateHandler();
         SpeedControl();
 
         // handle drag
@@ -60,6 +83,7 @@ public class RealityMovement : MonoBehaviour
     private void FixedUpdate()
     {
         MovePlayer();
+        Debug.Log(_moveSpeed);
     }
 
     // 
@@ -77,6 +101,50 @@ public class RealityMovement : MonoBehaviour
             
             Invoke(nameof(ResetJump), jumpCooldown); // continues to jump if space remains pressed
         }
+        
+        // start crouch
+        if (_grounded && Input.GetKeyDown(crouchKey))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);   // reduce scale
+            _rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);     // push down
+        }
+        
+        // stop crouch
+        if (Input.GetKeyUp(crouchKey))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, _startYScale, transform.localScale.z);
+        }
+    }
+
+
+    private void StateHandler()
+    {
+        // mode - Crouching
+        if (_grounded && Input.GetKey(crouchKey))
+        {
+            state = MovementState.Crouching;
+            _moveSpeed = crouchSpeed;
+        }
+        
+        // mode - Sprinting
+        else if (_grounded && Input.GetKey(sprintKey))
+        {
+            state = MovementState.Sprinting;
+            _moveSpeed = sprintSpeed;
+        }
+        
+        // mode - Walking
+        else if (_grounded)
+        {
+            state = MovementState.Walking;
+            _moveSpeed = walkSpeed;
+        }
+
+        // mode - Air
+        else
+        {
+            state = MovementState.Air;
+        }
     }
 
     //
@@ -87,9 +155,9 @@ public class RealityMovement : MonoBehaviour
         
         // differentiate movement on the ground and in air
         if(_grounded)
-            _rb.AddForce(_moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+            _rb.AddForce(_moveSpeed * 10f * _moveDirection.normalized, ForceMode.Force);
         else if(!_grounded)
-            _rb.AddForce(_moveDirection.normalized * moveSpeed * airMultiplier * 10f, ForceMode.Force);
+            _rb.AddForce(_moveSpeed * airMultiplier * 10f * _moveDirection.normalized, ForceMode.Force);
     }
 
     private void SpeedControl()
@@ -97,9 +165,9 @@ public class RealityMovement : MonoBehaviour
         Vector3 flatVel = new Vector3(_rb.velocity.x, 0f, _rb.velocity.z);
         
         // limit velocity if needed
-        if (flatVel.magnitude > moveSpeed)
+        if (flatVel.magnitude > _moveSpeed)
         {
-            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            Vector3 limitedVel = flatVel.normalized * _moveSpeed;
             _rb.velocity = new Vector3(limitedVel.x, _rb.velocity.y, limitedVel.z); // apply the new velocity to rb
         }
     }
@@ -109,7 +177,7 @@ public class RealityMovement : MonoBehaviour
         // reset y velocity
         _rb.velocity = new Vector3(_rb.velocity.x, 0f, _rb.velocity.z);
         
-        // create a vertical impulse force
+        // create an upward impulse force
         _rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
     }
 
