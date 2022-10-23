412defe6977e6c2901593f6c471f05c82dac7772
Alessio De Nicolò <alessio1.denicolo@mail.polimi.it>
2022-10-23 14:13:01 +0200

    Controller switched to rigid body

--- Assets/Code/Scripts/MouseLook.cs
+++ Assets/Code/Scripts/MouseLook.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
+    // link camera to body
+    public Transform orientation;
+    
+    
     // set mouse sensibility in X and Y axis
     [SerializeField] private float _xSensitivity = 100f;
     [SerializeField] private float _ySensitivity = 100f;
@@ -12,8 +16,8 @@ public class MouseLook : MonoBehaviour
     
     [SerializeField] private Transform _playerBody;
 
-    //private float _yaw;
-    private float _pitch = 0f;
+    private float _yRotation = 0f; // yaw movement variable
+    private float _xRotation = 0f; // pitch movement variable
   
 
 
@@ -29,19 +33,26 @@ public class MouseLook : MonoBehaviour
     {
         // get mouse input and proportionally modify the sensitivity
         float mouseX = Input.GetAxisRaw("Mouse X") * _xSensitivity * _sensitivity * Time.deltaTime;
-        float mouseY = - Input.GetAxisRaw("Mouse Y") * _ySensitivity * _sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * _ySensitivity * _sensitivity * Time.deltaTime;
         
-        //calculate the yaw (rotation around y) and the pitch (rotation around x)
-        //_yaw += mouseX;
-        _pitch += mouseY;
+        //calculate the rotation in both axis
+        _yRotation += mouseX;
+        _xRotation -= mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);    // Clamping allows to block the rotation
         
+        // rotate camera and orientation
+        transform.rotation = Quaternion.Euler(_xRotation, _yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, _yRotation, 0);
+        
+        
+        /*
         //Rotate the entire object (body + camera) around the Y axis
         _playerBody.transform.Rotate(new Vector3(0f, mouseX, 0f));
 
         //Rotate the camera on X axis
-        _pitch = Mathf.Clamp(_pitch, -90f, 90f);    // Clamping allows to block the rotation 
-        transform.localRotation = Quaternion.Euler(_pitch, 0f, 0f);
-        
+         
+        transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+        */
 
     }
 }
--- Assets/Code/Scripts/RealityMovement.cs
+++ Assets/Code/Scripts/RealityMovement.cs
@@ -2,67 +2,119 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class RealityMovement : MonoBehaviour
 {
-    [SerializeField] private CharacterController controller;
+    public float moveSpeed; // speed intensity
 
-    [SerializeField] private float _speed = 12f;
-    [SerializeField] private float _playerWeight = 1f;
-    [SerializeField] private float _jumpHeight = 3f;
-    [SerializeField] private float _groundDistance = 0.4f;
-    [SerializeField] private float _jumpMovementLimitationRate = 10;
-    [SerializeField] private Transform _groundCheck; //This object is at the bottom of the player object, it's needed to check if the player is jumping or not
-    [SerializeField] private LayerMask _groundMask;
+    // set the ground check
+    public float groundDrag;
+    public float playerHeight;
+    public LayerMask ground;
+    private bool _grounded;
     
-    private float _gravity = -9.81f;
-    private Vector3 _velocity;
-    private Transform _transform;
+    // set jump
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    private bool _readyToJump;
+    
+    // set the jump key
+    public KeyCode jumpKey = KeyCode.Space;
 
-    private void Awake()
+    public Transform orientation;
+
+    private float _horizontalInput;
+    private float _verticalInput;
+
+    private Vector3 _moveDirection;
+
+    private Rigidbody _rb;
+
+
+    // Start is called before the first frame update
+    private void Start()
     {
-        _transform = GetComponent<Transform>();
+        _rb = GetComponent<Rigidbody>();
+        _rb.freezeRotation = true;
+        
+        ResetJump(); // set readyToJump to "true"
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        // isGrounded is true if there are any colliders overlapping the sphere defined by _groundCheck.position and _groundDistance in world coordinates
-        bool isGrounded = Physics.CheckSphere(_groundCheck.position, _groundDistance, _groundMask);
-        
-        // This check stabilizes the player on the ground
-        if (isGrounded && _velocity.y < 0)
-        {
-            _velocity.y = -2f;
-        }
-        
-        // Take the input form the keyboard
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        _grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, ground);
 
-        // Vector that describe the movement relative to the player oriantation
-        Vector3 move = _transform.right * x + _transform.forward * z;
-        
-        // The player jump with the space key
-        if(Input.GetButtonDown("Jump") && isGrounded)
-        {
-            _velocity.y = Mathf.Sqrt(_jumpHeight * -2f * _gravity * _playerWeight);
-        }
-        
-        // Calculate the velocity on y axis
-        _velocity.y += _gravity * _playerWeight * Time.deltaTime;
-        move.y = _velocity.y;
-        
-        // different type of movement at the ground and in the midair
-        if (isGrounded)
-        {
-            controller.Move(move * (_speed * Time.deltaTime));
-        }
+        MyInput();
+        SpeedControl();
+
+        // handle drag
+        if (_grounded)
+            _rb.drag = groundDrag;
         else
-        {
-            controller.Move(move/_jumpMovementLimitationRate * (_speed * Time.deltaTime));
-        }
+            _rb.drag = 0;
+    }
 
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    // 
+    private void MyInput()
+    {
+        _horizontalInput = Input.GetAxisRaw("Horizontal");
+        _verticalInput = Input.GetAxisRaw("Vertical");
         
+        // when to jump
+        if (Input.GetKey(jumpKey) && _readyToJump && _grounded)
+        {
+            _readyToJump = false;
+            
+            Jump();
+            
+            Invoke(nameof(ResetJump), jumpCooldown); // continues to jump if space remains pressed
+        }
+    }
+
+    //
+    private void MovePlayer()
+    {
+        // calculate movement direction
+        _moveDirection = orientation.forward * _verticalInput + orientation.right * _horizontalInput;
+        
+        // differentiate movement on the ground and in air
+        if(_grounded)
+            _rb.AddForce(_moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        else if(!_grounded)
+            _rb.AddForce(_moveDirection.normalized * moveSpeed * airMultiplier * 10f, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(_rb.velocity.x, 0f, _rb.velocity.z);
+        
+        // limit velocity if needed
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            _rb.velocity = new Vector3(limitedVel.x, _rb.velocity.y, limitedVel.z); // apply the new velocity to rb
+        }
+    }
+
+    private void Jump()
+    {
+        // reset y velocity
+        _rb.velocity = new Vector3(_rb.velocity.x, 0f, _rb.velocity.z);
+        
+        // create a vertical impulse force
+        _rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        _readyToJump = true;
     }
 }
