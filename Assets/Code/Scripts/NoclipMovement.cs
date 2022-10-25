e483243148e62b2b1d547f18e78ae6f83e3383f0
stefanofossati <10569836@mail.polimi.it>
2022-10-25 13:56:34 +0200

    Rename files and create prefabs

--- Assets/Code/Scripts/NoclipMovement.cs
+++ Assets/Code/Scripts/NoclipMovement.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NoClipMovement : MonoBehaviour
+public class NoclipMovement : MonoBehaviour
 {
 
     [SerializeField]
