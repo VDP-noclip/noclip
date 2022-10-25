2fd7a38c53c7fde729a9b666810430b676a77a42
Stefano Fossati <79704488+stefanofossati@users.noreply.github.com>
2022-10-25 13:34:13 +0200

    advanced player controller with rigidbody (#16)
    Change of the reality mode player body:
    -  mesh with a controller script that manages the physics -> rigidbody 
    - separation of the camera from the reality mode player body
    
    Co-authored-by: stefanofossati <10569836@mail.polimi.it>
    Co-authored-by: Alessio De Nicolò <alessio1.denicolo@mail.polimi.it>
    Co-authored-by: Nicola Ischia <36990714+spig95@users.noreply.github.com>

--- Assets/Code/Scripts/MouseLook.cs
+++ Assets/Code/Scripts/MouseLook.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
+    // link camera to body
+    [SerializeField] private Transform _orientation;
+    
+    
     // set mouse sensibility in X and Y axis
     [SerializeField] private float _xSensitivity = 100f;
     [SerializeField] private float _ySensitivity = 100f;
     [SerializeField] private float _sensitivity = 1f;
     
-    [SerializeField] private Transform _playerBody;
+    private Transform _transform;
 
-    //private float _yaw;
-    private float _pitch = 0f;
-  
+    private float _yRotation = 0f; // yaw movement variable
+    private float _xRotation = 0f; // pitch movement variable
 
+
+    private void Awake()
+    {
+        _transform = GetComponent<Transform>();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -29,19 +37,26 @@ public class MouseLook : MonoBehaviour
     {
         // get mouse input and proportionally modify the sensitivity
         float mouseX = Input.GetAxisRaw("Mouse X") * _xSensitivity * _sensitivity * Time.deltaTime;
-        float mouseY = - Input.GetAxisRaw("Mouse Y") * _ySensitivity * _sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * _ySensitivity * _sensitivity * Time.deltaTime;
         
-        //calculate the yaw (rotation around y) and the pitch (rotation around x)
-        //_yaw += mouseX;
-        _pitch += mouseY;
+        //calculate the rotation in both axis
+        _yRotation += mouseX;
+        _xRotation -= mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);    // Clamping allows to block the rotation
         
+        // rotate camera and orientation
+        _transform.rotation = Quaternion.Euler(_xRotation, _yRotation, 0);
+        _orientation.rotation = Quaternion.Euler(0, _yRotation, 0);
+        
+        
+        /*
         //Rotate the entire object (body + camera) around the Y axis
         _playerBody.transform.Rotate(new Vector3(0f, mouseX, 0f));
 
         //Rotate the camera on X axis
-        _pitch = Mathf.Clamp(_pitch, -90f, 90f);    // Clamping allows to block the rotation 
-        transform.localRotation = Quaternion.Euler(_pitch, 0f, 0f);
-        
+         
+        transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+        */
 
     }
 }
--- Assets/Code/Scripts/RealityMovement.cs
+++ Assets/Code/Scripts/RealityMovement.cs
@@ -1,53 +1,195 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RealityMovement : MonoBehaviour
 {
-    public CharacterController controller;
-
-    [SerializeField] private float _speed = 12f;
-    [SerializeField] private float _gravity = -9.81f;
-    [SerializeField] private float _jumpHeight = 3f;
-    [SerializeField] private float _groundDistance = 0.4f;
-
-    [SerializeField] private Transform _groundCheck;
+    private enum MovementState       // define player states
+    {
+        Walking,
+        Sprinting,
+        Crouching,
+        Air
+    }
     
-    [SerializeField] private LayerMask _groundMask;
+    [Header("Speed")]
+    [SerializeField] private float _walkSpeed = 6f;
+    [SerializeField] private float _sprintSpeed = 10f;
     
-    private Vector3 _velocity;
+    private float _moveSpeed;     // speed intensity
+    
+    [Header("Drag")]
+    [SerializeField] private float _groundDrag = 4f;    // ground drag
+    
+    [Header("Jump")]
+    [SerializeField] private float _jumpForce = 8f;     // set jump upward force
+    [SerializeField] private float _jumpCooldown = 0.25f;      // set jump cooldown
+    [SerializeField] private float _airMultiplier = 0.4f;     // set air movement limitation
+    private bool _readyToJump;      //
+
+    [Header("Crouch")]
+    [SerializeField] private float _crouchSpeed = 2f;
+    [SerializeField] private float _crouchYScale = 0.5f;
+    private float _startYScale;
+    
+    [Header("Keybinds")]
+    [SerializeField] private KeyCode jumpKey = KeyCode.Space;     // set the jump key
+    [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;   // set the sprint key
+    [SerializeField] private KeyCode crouchKey = KeyCode.LeftControl;
+
+    [Header("Ground Check")]
+    // set the ground check
+    [SerializeField] private float _playerHeight = 2f;
+    [SerializeField] private LayerMask _ground;
+    private bool _grounded;
+    
+    [SerializeField] private Transform _orientation;
+
+    private float _horizontalInput;
+    private float _verticalInput;
+
+    private Vector3 _moveDirection;
+
     private Transform _transform;
+    private Rigidbody _rigidbody;      // set the rigidbody
+
+    // player states
+    [SerializeField] private MovementState _state;     // current player state
 
     private void Awake()
     {
+        _rigidbody = GetComponent<Rigidbody>();
         _transform = GetComponent<Transform>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // Start is called before the first frame update
+    private void Start()
     {
-        bool isGrounded = Physics.CheckSphere(_groundCheck.position, _groundDistance, _groundMask);
+        _rigidbody.freezeRotation = true;
+        
+        ResetJump(); // set _readyToJump to "true"
 
-        if (isGrounded && _velocity.y < 0)
+        _startYScale = _transform.localScale.y;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        _grounded = Physics.Raycast(_transform.position, Vector3.down, _playerHeight * 0.5f + 0.2f, _ground);
+
+        UserInput();
+        StateHandler();
+        SpeedControl();
+
+        // handle drag
+        if (_grounded)
+            _rigidbody.drag = _groundDrag;
+        else
+            _rigidbody.drag = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+        //Debug.Log(_moveSpeed);
+    }
+
+    // 
+    private void UserInput()
+    {
+        _horizontalInput = Input.GetAxisRaw("Horizontal");
+        _verticalInput = Input.GetAxisRaw("Vertical");
+        
+        // when to jump
+        if (Input.GetKey(jumpKey) && _readyToJump && _grounded)
         {
-            _velocity.y = -2f;
+            _readyToJump = false;
+            
+            Jump();
+            
+            Invoke(nameof(ResetJump), _jumpCooldown); // continues to jump if space remains pressed
+        }
+        
+        // start crouch
+        if (_grounded && Input.GetKeyDown(crouchKey))
+        {
+            _transform.localScale = new Vector3(_transform.localScale.x, _crouchYScale, _transform.localScale.z);   // reduce scale
+            _rigidbody.AddForce(Vector3.down * 5f, ForceMode.Impulse);     // push down
+        }
+        
+        // stop crouch
+        if (Input.GetKeyUp(crouchKey))
+        { 
+            _transform.localScale = new Vector3(_transform.localScale.x, _startYScale, _transform.localScale.z);
+        }
+    }
+
+
+    private void StateHandler()
+    {
+        // mode - Crouching
+        if (_grounded && Input.GetKey(crouchKey))
+        {
+            _state = MovementState.Crouching;
+            _moveSpeed = _crouchSpeed;
+        }
+        
+        // mode - Sprinting
+        else if (_grounded && Input.GetKey(sprintKey))
+        {
+            _state = MovementState.Sprinting;
+            _moveSpeed = _sprintSpeed;
+        }
+        
+        // mode - Walking
+        else if (_grounded)
+        {
+            _state = MovementState.Walking;
+            _moveSpeed = _walkSpeed;
         }
 
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        Vector3 move = _transform.right * x + _transform.forward * z;
-
-        controller.Move(move * (_speed * Time.deltaTime));
-
-        if(Input.GetButtonDown("Jump") && isGrounded)
+        // mode - Air
+        else
         {
-            _velocity.y = Mathf.Sqrt(_jumpHeight * -2f * _gravity);
+            _state = MovementState.Air;
         }
+    }
 
-        _velocity.y += _gravity * Time.deltaTime;
+    //
+    private void MovePlayer()
+    {
+        // calculate movement direction
+        _moveDirection = _orientation.forward * _verticalInput + _orientation.right * _horizontalInput;
+        
+        // differentiate movement on the ground and in air
+        if (_grounded)
+            _rigidbody.AddForce(_moveSpeed * 10f * _moveDirection.normalized, ForceMode.Force);
+        else
+            _rigidbody.AddForce(_moveSpeed * _airMultiplier * 10f * _moveDirection.normalized, ForceMode.Force);
+    }
 
-        controller.Move(_velocity * Time.deltaTime);
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(_rigidbody.velocity.x, 0f, _rigidbody.velocity.z);
+        
+        // limit velocity if needed
+        if (flatVel.magnitude > _moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * _moveSpeed;
+            _rigidbody.velocity = new Vector3(limitedVel.x, _rigidbody.velocity.y, limitedVel.z); // apply the new velocity to rb
+        }
+    }
+
+    private void Jump()
+    {
+        // reset y velocity
+        _rigidbody.velocity = new Vector3(_rigidbody.velocity.x, 0f, _rigidbody.velocity.z);
+        
+        // create an upward impulse force
+        _rigidbody.AddForce(_transform.up * _jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        _readyToJump = true;
     }
 }
