75b79f775661a04d6633cd16ffc8a627fa3453c8
Stefano Fossati <79704488+stefanofossati@users.noreply.github.com>
2022-11-06 19:01:49 +0100

    Reality player Movement (#51)
    Main corrections:
    
    - Check the problems of the last controller version
    - Find solution and check bugs
    
    Co-authored-by: stefanofossati <10569836@mail.polimi.it>
    Co-authored-by: TizioMaurizio2 <maurizio.vetere@polispace.it>

--- Assets/Code/Scripts/NoclipRealityManagement/RealityMovement.cs
+++ Assets/Code/Scripts/NoclipRealityManagement/RealityMovement.cs
@@ -1,70 +1,8 @@
+using System;
 using UnityEngine;
 
 public class RealityMovement : MonoBehaviour
 {
-    [Header("Speed")]
-    [SerializeField] private float _walkSpeed = 6f;
-    [SerializeField] private float _sprintSpeed = 10f;
-    
-    private float _moveSpeed;     // speed intensity
-    
-    [Header("Drag")]
-    [SerializeField] private float _groundDrag = 4f;    // ground drag
-    
-    [Header("Jump")]
-    [SerializeField] private float _jumpForce = 8f;     // set jump upward force
-    [SerializeField] private float _gravity = 20f;      // set gravity
-    [SerializeField] private float _jumpCooldown = 0.25f;      // set jump cooldown
-    [SerializeField] private float _airMultiplier = 0.4f;     // set air movement limitation
-    private bool _readyToJump;
-
-    [Header("Crouch")]
-    [SerializeField] private float _crouchSpeed = 2f;
-    [SerializeField] private float _crouchYScale = 0.5f;
-    private float _startYScale;
-    
-    [Header("Keybinds")]
-    [SerializeField] private KeyCode jumpKey = KeyCode.Space;     // set the jump key
-    [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;   // set the sprint key
-    [SerializeField] private KeyCode crouchKey = KeyCode.LeftControl;
-
-    [Header("Ground check")]
-    // set the ground check
-    [SerializeField] private float _playerHeight = 2f;
-    [SerializeField] private LayerMask _ground;
-    private bool _grounded;
-
-    [Header("Slope handling")]
-    [SerializeField] private float _maxSlopeAngle;
-    private RaycastHit _slopeHit;
-    private bool _exitingSlope;
-    
-    
-    // It's true if is the realbody, it's false if is the noclip body
-    private bool _currentPlayerBody = true;
-    
-    private float _horizontalInput;
-    private float _verticalInput;
-
-    private Vector3 _moveDirection;
-
-    private Transform _transform;
-    private Rigidbody _rigidbody;      // set the rigidbody
-
-    // player states
-    [SerializeField] private MovementState _state;     // current player state
-
-    //sticking problem with walls was solved by removing collider friction alltogether and reliying only on RealityPlayer's drag, aka how it was probably intended from the beginning
-    //TOREMOVE reality body capsule collider to change its material and fix sticking to walls
-    //private CapsuleCollider _realityBodyCollider;
-
-    //slope gravity vector3
-    private Vector3 _slopeGravity = Vector3.zero;
-    private bool _bodyOnSlope = false;
-    private bool _groundedOnSlope= false;
-    private bool _touchingWall = false;
-
-    [SerializeField] private bool Grounded;
     private enum MovementState       // define player states
     {
         Walking,
@@ -73,24 +11,77 @@ public class RealityMovement : MonoBehaviour
         Air
     }
     
+    [Header("Speed")]
+    [SerializeField] private float _runSpeed = 6f;
+    [SerializeField] private float _walkSpeed = 3f;
+    
+    private float _moveSpeed;     // speed intensity
+    
+    [Header("Drag")]
+    [SerializeField] private float _groundDrag = 4f;    // ground drag
+    
+    [Header("Jump")]
+    [SerializeField] private float _jumpForce = 8f;     // set jump upward force
+    [SerializeField] private float _jumpCooldown = 0.25f;      // set jump cooldown
+    [SerializeField] private float _airMultiplier = 0.4f;     // set air movement limitation
+    private bool _readyToJump;      //
+
+    [Header("Crouch")]
+    [SerializeField] private float _crouchSpeed = 2f;
+    [SerializeField] private float _crouchYScale = 0.5f;
+    private float _startYScale;
+    
+    [Header("Slope handling")]
+    [SerializeField] private float _maxSlopeAngle;
+    private RaycastHit _slopeHit;
+    private bool _exitingOnSlope = false;
+    
+    [Header("Keybinds")]
+    [SerializeField] private KeyCode _jumpKey = KeyCode.Space;     // set the jump key
+    [SerializeField] private KeyCode _sprintKey = KeyCode.LeftShift;   // set the sprint key
+    [SerializeField] private KeyCode _crouchKey = KeyCode.LeftControl;
+
+    [Header("Ground Check")]
+    // set the ground check
+    
+    // stefanofossati comment: I prefer to have a groundCheck object instead of the player height
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private float _playerHeight = 2f;  // Actually this isn't used
+    [SerializeField] private LayerMask _ground;
+    private bool _grounded;
+
+    [SerializeField] private float _gravityMultiplier = 1f;
+    [SerializeField] private Transform _orientation;
+
+    private float _gravity = 9.81f;  // This is used for the movement force 
+    private float _horizontalInput;
+    private float _verticalInput;
+
+    private Vector3 _moveDirection;
+
+    private Transform _transform;
+    private Rigidbody _rigidbody;      // set the rigidbody
+
+    // It's true if is the realbody, it's false if is the noclip body
+    private bool _currentPlayerBody  = true;
+
+    // player states
+    [SerializeField] private MovementState _state;     // current player state
+
+    [SerializeField] private bool _OnSlope; //This seriazlized field is only use to debug from unity // TODO remove
+    [SerializeField] private bool Grounded; //This seriazlized field is only use to debug from unity // TODO remove
+    
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
-        _rigidbody.AddForce(_slopeGravity, ForceMode.Force);
+        Physics.gravity *= _gravityMultiplier;   // Change the gravity
         _transform = GetComponent<Transform>();
-
-        //TOREMOVE
-        //_realityBodyCollider = GetComponentInChildren<CapsuleCollider>();
-        //set reality body collider material to no_friction material in Assets/Physic Materials
-        //_realityBodyCollider.material = (PhysicMaterial) Resources.Load("Physic Materials/no_friction");
     }
 
     // Start is called before the first frame update
     private void Start()
     {
-        //get gravity vector
-        Physics.gravity = Vector3.up * -_gravity;
-        
+
         _rigidbody.freezeRotation = true;
         
         ResetJump(); // set _readyToJump to "true"
@@ -101,64 +92,41 @@ public class RealityMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        Grounded = _grounded;
         if (_currentPlayerBody)
         {
-            _grounded = Physics.Raycast(_transform.position, Vector3.down, _playerHeight * 0.5f + 0.2f, _ground);
-
+            //Alternative 1
+            //_grounded = Physics.Raycast(_transform.position,  Vector3.down, _playerHeight * 0.5f + 0.3f, _ground);
+        
+            //Alternative 2
+            // ATTENTION!!! the 0.48f value is based on the player capsule radius
+            _grounded = Physics.CheckSphere(_groundCheck.position,  0.48f, _ground);
+        
+            // stefanofossati comment: I Think that this second alternative is a little more precise for the slopes
+            _OnSlope = OnSlope(); // used to debug // TODO remove
+            Grounded = _grounded; // used to debug // TODO remove
+        
             UserInput();
             StateHandler();
+            MovePlayer();
             SpeedControl();
 
             // handle drag
-            if (_grounded || _groundedOnSlope) //needed to make slopes higher than 45 degrees feel not slippery but to jump there UserInput should be modified
-            //IMPORTANT slopes higher than about 80 degrees are fundamentally broken because they enable drag free movement
-            //but are still generating binding reaction (reazione vincolare), that is because the function that checks
-            //if player is on a slope doesn't return a normal vector if the slope angle is that high.
-            //80 degrees slopes and walls are different because there is no way to exploit a wall to accelerate while in air
-            {
+            if (_grounded)
                 _rigidbody.drag = _groundDrag;
-                //Frictionless(false);
-            }
             else
-            {
-                /*if(_touchingWall){
-                    _rigidbody.drag = _groundDrag/2;
-                    _touchingWall = false; //consume touchingwall event
-                }
-                else*/
-                    _rigidbody.drag = 0;
-            }
+                _rigidbody.drag = 0;
         }
     }
 
-    /*private void Frictionless(bool frictionless) //TOREMOVE
+    public void ResetSpeedOnRespawn()
     {
-        if (frictionless)
-            _realityBodyCollider.material = (PhysicMaterial) Resources.Load("Physic Materials/no_friction");
-        else
-            _realityBodyCollider.material = (PhysicMaterial) Resources.Load("Physic Materials/default");
-    }*/
-
-    private void FixedUpdate()
-    {
-        MovePlayer();
-        //Debug.Log(_moveSpeed);
+        _rigidbody.velocity = Vector3.zero;
     }
     
     public void ActivatePlayer(bool active)
     {
         _currentPlayerBody = active;
     }
-
-    /*private void OnCollisionStay(Collision collision) {
-        //if colliding with a ground object set drag to ground drag
-        //collision.gameObject Layer get name
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
-        {
-            _touchingWall = true;
-        }
-    }*/
 
     // 
     private void UserInput()
@@ -167,7 +135,7 @@ public class RealityMovement : MonoBehaviour
         _verticalInput = Input.GetAxisRaw("Vertical");
         
         // when to jump
-        if (Input.GetKey(jumpKey) && _readyToJump && _grounded) //_grounded is true up to 45 degrees, so _groundedOnSlope together with slope angle should be checked to jump at higher slopes
+        if (Input.GetKey(_jumpKey) && _readyToJump && _grounded)
         {
             _readyToJump = false;
             
@@ -177,42 +145,41 @@ public class RealityMovement : MonoBehaviour
         }
         
         // start crouch
-        if (_grounded && Input.GetKeyDown(crouchKey))
+        if (_grounded && Input.GetKeyDown(_crouchKey))
         {
             _transform.localScale = new Vector3(_transform.localScale.x, _crouchYScale, _transform.localScale.z);   // reduce scale
             _rigidbody.AddForce(Vector3.down * 5f, ForceMode.Impulse);     // push down
         }
         
         // stop crouch
-        if (Input.GetKeyUp(crouchKey))
+        if (Input.GetKeyUp(_crouchKey))
         { 
             _transform.localScale = new Vector3(_transform.localScale.x, _startYScale, _transform.localScale.z);
         }
     }
-    
 
 
     private void StateHandler()
     {
         // mode - Crouching
-        if (_grounded && Input.GetKey(crouchKey))
+        if (_grounded && Input.GetKey(_crouchKey))
         {
             _state = MovementState.Crouching;
             _moveSpeed = _crouchSpeed;
         }
         
         // mode - Sprinting
-        else if (_grounded && Input.GetKey(sprintKey))
+        else if (_grounded && Input.GetKey(_sprintKey))
         {
             _state = MovementState.Sprinting;
-            _moveSpeed = _sprintSpeed;
+            _moveSpeed = _walkSpeed;
         }
         
         // mode - Walking
         else if (_grounded)
         {
             _state = MovementState.Walking;
-            _moveSpeed = _walkSpeed;
+            _moveSpeed = _runSpeed;
         }
 
         // mode - Air
@@ -226,78 +193,39 @@ public class RealityMovement : MonoBehaviour
     private void MovePlayer()
     {
         // calculate movement direction
-        _moveDirection = _rigidbody.transform.forward * _verticalInput + _rigidbody.transform.right * _horizontalInput;
-        
-        /* Tizio - Stefano can you tell me how this works?
-        // on slope
-        if (OnSlope() && !_exitingSlope)
-        {
-            _rigidbody.AddForce(GetSlopeMoveDirection() * _moveSpeed * 20f, ForceMode.Force);
+        _moveDirection = _orientation.forward * _verticalInput + _orientation.right * _horizontalInput;
 
-            if (_rigidbody.velocity.y > 0)
-                _rigidbody.AddForce(Vector3.down * 80f, ForceMode.Force);
-        }*/
-        
+        // onslope
+        if (OnSlope() && !_exitingOnSlope)
+        {
+            // Add a force on the plane direction of the plane
+            _rigidbody.AddForce(GetSlopeMoveDirection() * (_moveSpeed * _gravity), ForceMode.Force); 
+            
+            // if _rigidbody.velocity.y != 0 and w a s or d pressed
+            if (_rigidbody.velocity.y != 0 &&(_horizontalInput != 0 || _verticalInput != 0))
+            {
+                // Add a force that obliged the player to stay on the inclined plane 
+                _rigidbody.AddForce(Vector3.down * (_gravity * _gravityMultiplier * 4), ForceMode.Force);  
+            }
+        }
         // differentiate movement on the ground and in air
         if (_grounded)
-            _rigidbody.AddForce(_moveSpeed * 10f * _moveDirection.normalized, ForceMode.Force);
+            _rigidbody.AddForce(_moveSpeed * _gravity * _moveDirection.normalized, ForceMode.Force);
         else
-            _rigidbody.AddForce(_moveSpeed * _airMultiplier * 10f * _moveDirection.normalized, ForceMode.Force);
-        
-        // turn gravity off while on slope
-        //tizio: it is actually better to change the friction instead, to avoid side effects
-        //_rigidbody.useGravity = !OnSlope();
+            _rigidbody.AddForce(_moveSpeed * _airMultiplier * _gravity * _moveDirection.normalized, ForceMode.Force);
 
-        SlopeHandler();
-        
-    }
-
-    private void SlopeHandler() //works up to 45 degrees because above something strange happens with cosine, ANYWAY player is not supposed to climb such slopes, he's not a steinbock
-    {
-        /*if(_touchingWall){
-            _rigidbody.drag = _groundDrag/2;
-            _touchingWall = false; //consume touchingwall event
-        }*/
-        if (Physics.Raycast(_transform.position, Vector3.down, out _slopeHit, _playerHeight * 0.5f + 0.3f))
-        {
-            float angle = Vector3.Angle(Vector3.up, _slopeHit.normal);
-            _groundedOnSlope = true;
-            //debug angle
-            //Debug.Log("slope angle: " + angle);
-            if(angle > 0 && angle < _maxSlopeAngle && !_bodyOnSlope){
-                _bodyOnSlope = true;
-                _slopeGravity = _slopeHit.normal * Physics.gravity.magnitude;
-                //Debug.Log("_slopeGravity: " + _slopeGravity.magnitude);
-                _rigidbody.useGravity = false;
-            }
-            else if (!_bodyOnSlope){
-                _bodyOnSlope = false;
-                _slopeGravity = Vector3.zero;
-                _rigidbody.useGravity = true;
-            }
-            //the lack of gravity makes the player slide more when the surface is more inclined
-        }
-        else{
-            _groundedOnSlope = false;
-            _bodyOnSlope = false;
-            _slopeGravity = Vector3.zero;
-            _rigidbody.useGravity = true;
-            //_groundDrag = 8;
-        }
-        //log _grounded
-        //Debug.Log("grounded: " + _grounded);
-        //Debug.Log("groundedonslope: " + _groundedOnSlope);
+        _rigidbody.useGravity = !OnSlope();
     }
 
     private void SpeedControl()
     {
-        // limiting speed on slope
-        if (OnSlope() && !_exitingSlope)
+        if (OnSlope() && !_exitingOnSlope)
         {
             if (_rigidbody.velocity.magnitude > _moveSpeed)
+            {
                 _rigidbody.velocity = _rigidbody.velocity.normalized * _moveSpeed;
+            }
         }
-        // limiting speed on ground or in air
         else
         {
             Vector3 flatVel = new Vector3(_rigidbody.velocity.x, 0f, _rigidbody.velocity.z);
@@ -309,12 +237,13 @@ public class RealityMovement : MonoBehaviour
                 _rigidbody.velocity = new Vector3(limitedVel.x, _rigidbody.velocity.y, limitedVel.z); // apply the new velocity to rb
             }
         }
+        
     }
 
     private void Jump()
     {
-        _exitingSlope = true;
         
+        _exitingOnSlope = true;
         // reset y velocity
         _rigidbody.velocity = new Vector3(_rigidbody.velocity.x, 0f, _rigidbody.velocity.z);
         
@@ -326,22 +255,22 @@ public class RealityMovement : MonoBehaviour
     {
         _readyToJump = true;
         
-        _exitingSlope = false;
+        _exitingOnSlope = false;
     }
 
+    //This function check if the plane is inclined
     private bool OnSlope()
     {
-        if (Physics.Raycast(_transform.position, Vector3.down, out _slopeHit, _playerHeight * 0.5f + 0.3f))
+        if (Physics.Raycast(_groundCheck.position, Vector3.down, out _slopeHit, 0.5f + 0.3f))
         {
             float angle = Vector3.Angle(Vector3.up, _slopeHit.normal);
-            //debug angle
-            //Debug.Log(angle);
-            return angle < _maxSlopeAngle && angle != 0;
-            //the lack of gravity makes the player slide more when the surface is more inclined
+            return angle < _maxSlopeAngle && angle != 0 && _state!=MovementState.Air;
         }
+
         return false;
     }
-    
+
+    //This function retuns the vector3 that represents the plane on which the player is moving
     private Vector3 GetSlopeMoveDirection()
     {
         return Vector3.ProjectOnPlane(_moveDirection, _slopeHit.normal).normalized;
