452c8eb146825d06b41484d119471f74f5ceacb7
spig95 <nicolaischia95@gmail.com>
2022-11-06 21:28:19 +0100

    Additive scenes organization

---
+++ Assets/Code/Scripts/Utils/PuzzleManager.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _puzzlePrefabs;
+
+    private int _currentPuzzleIndex = 0;
+
+    private void Awake()
+    {
+        LoadNextPuzzle();
+    }
+
+    void Update()
+    {   
+        // Todo: execute these 2 lines only when the player goes on the checkpoint
+        if (Application.isEditor && Input.GetKeyDown(KeyCode.X))
+        {
+            LoadNextPuzzle();
+            GameObject.Find("RealityPlayer").GetComponent<NoclipManager>().FindNoClipObjControllers();
+        }
+    }
+
+    private void LoadNextPuzzle()
+    {
+        GameObject.Instantiate(_puzzlePrefabs[_currentPuzzleIndex]);
+        _currentPuzzleIndex += 1;
+    }
+}
