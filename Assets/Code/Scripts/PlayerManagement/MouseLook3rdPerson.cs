212655a93105673b00360a1db52ed74980cedadd
TizioMaurizio <maurizio.vetere@mail.polimi.it>
2022-11-14 10:48:20 +0100

    third person switch on T key

--- Assets/Code/Scripts/PlayerManagement/MouseLook3rdPerson.cs
+++ Assets/Code/Scripts/PlayerManagement/MouseLook3rdPerson.cs
@@ -20,42 +20,56 @@ public class MouseLook3rdPerson : MonoBehaviour
         //initialize player to parent of parent
         _player = GameObject.Find("RealityPlayer");
     }
+
+    private bool _thirdPerson = true;
+
+    // Update is called once per frame
     void Update()
     {
-        //if wheel is scrolled
-        if (Input.GetAxis("Mouse ScrollWheel") != 0)
+        //press t to do all this
+        if (Input.GetKeyDown(KeyCode.T))
         {
-            //if wheel is scrolled up
-            if (Input.GetAxis("Mouse ScrollWheel") > 0)
-            {
-                //if distance is greater than 1
-                if (_distance > 10)
-                    //set distance to 0
-                    _distance-=10;
-                else if (_distance > 1)
-                    //decrease distance by 1
-                    _distance--;
-            }
-            //if wheel is scrolled down
-            else
-            {
-                if(_distance < 100)
-                    //if distance is less than 10
-                    if (_distance < 10)
-                        //increase distance by 1
-                        _distance++;
-                    else
-                        //increase distance by 10
-                        _distance += 10;
-            }
+            _thirdPerson = !_thirdPerson;
+            //place camera at player + 0.6f on y axis
+            _camera.position = _player.transform.position + new Vector3(0, 0.6f, 0);
         }
-        //move camera to the center of the player
-        _camera.position = _player.transform.position;
-        //get direction of the camera
-        Vector3 direction = _camera.forward;
-        //move camera back in direction
-        _camera.position -= direction * _distance;
-        //rotate camera to look at player
-        _camera.LookAt(_player.transform);
+        if (_thirdPerson)
+        {
+            //if wheel is scrolled
+            if (Input.GetAxis("Mouse ScrollWheel") != 0)
+            {
+                //if wheel is scrolled up
+                if (Input.GetAxis("Mouse ScrollWheel") > 0)
+                {
+                    //if distance is greater than 1
+                    if (_distance > 10)
+                        //set distance to 0
+                        _distance-=10;
+                    else if (_distance > 1)
+                        //decrease distance by 1
+                        _distance--;
+                }
+                //if wheel is scrolled down
+                else
+                {
+                    if(_distance < 100)
+                        //if distance is less than 10
+                        if (_distance < 10)
+                            //increase distance by 1
+                            _distance++;
+                        else
+                            //increase distance by 10
+                            _distance += 10;
+                }
+            }
+            //move camera to the center of the player
+            _camera.position = _player.transform.position;
+            //get direction of the camera
+            Vector3 direction = _camera.forward;
+            //move camera back in direction
+            _camera.position -= direction * _distance;
+            //rotate camera to look at player
+            _camera.LookAt(_player.transform);
+        }
     }
 }
