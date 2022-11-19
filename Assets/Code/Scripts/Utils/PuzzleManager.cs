b6b35f60b36660add843465566856561e9ecec30
Nicola Ischia <36990714+spig95@users.noreply.github.com>
2022-11-19 12:27:07 +0100

    Improve game manager and puzzle manager code (#79)
    ### Game Manager
    @TizioMaurizio as discussed, I removed the additive scene loader
    component from the game manager and I rely uniquely on the list
    `_gameAreas`.
    
    Moreover, I have added the `GameCompleted` state (I think you already
    added it, and I removed it by mistake). We are in this state when we
    finish the last area in `_gameAreas`.
    
    ### Puzzle Manager
    Here I made a minor improvement. Instead of using an exception, I check
    when we reach the final puzzle index and act accordingly. I think the
    code is easier to read and to maintain in case we find bugs.

--- Assets/Code/Scripts/Utils/PuzzleManager.cs
+++ Assets/Code/Scripts/Utils/PuzzleManager.cs
@@ -8,21 +8,41 @@ public class PuzzleManager : MonoBehaviour
     [SerializeField] private GameObject[] _puzzlePrefabs;
 
     private int _currentPuzzleIndex;
+    private int _finalPuzzleIndex;
 
     private GameObject _gameManager;
     private Vector3 _puzzlePosition;
     
     private void Awake()
     {
-        LoadNextPuzzle();
+        LoadCurrentPuzzle();
+        _finalPuzzleIndex = _puzzlePrefabs.Length - 1;
     }
-
+    
+    /// <summary>
+    /// Call this function when the puzzle is finished and you need to move to the next one. If the current puzzle is
+    /// the last one, set the game state to AreaFinished
+    /// </summary>
     public void LoadNextPuzzle()
     {
-        //NOTE puzzles don't snap perfectly between anchors probably because of rotations
-        try{
-            GameObject newPuzzle = Instantiate(_puzzlePrefabs[_currentPuzzleIndex]);
+        if (_currentPuzzleIndex != _finalPuzzleIndex)
+        {
             _currentPuzzleIndex += 1;
+            LoadCurrentPuzzle();
+            GameObject.Find("RealityPlayer").GetComponent<NoclipManager>().FindNoClipObjControllers();
+        }
+        else
+        {
+            Debug.Log("No more puzzles to load. Area finished!");
+            GameObject.Find("GameManager").GetComponent<GameManager>().SetAreaFinished();
+        }
+    }
+
+    private void LoadCurrentPuzzle()
+    {
+            //NOTE: puzzles don't snap perfectly between anchors probably because of rotations
+            GameObject newPuzzle = Instantiate(_puzzlePrefabs[_currentPuzzleIndex]);
+            
             //rotate newPuzzle.transform.Find("BeginAnchor") to zero
             //newPuzzle.transform.Find("BeginAnchor").transform.rotation = Quaternion.Euler(0, 0, 0);
             //find begin anchor
@@ -55,15 +75,5 @@ public class PuzzleManager : MonoBehaviour
             pole.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
             pole.GetComponent<Renderer>().material.color = Color.green;
             */
-            
-            if (_currentPuzzleIndex > 1)
-                GameObject.Find("RealityPlayer").GetComponent<NoclipManager>().FindNoClipObjControllers();
-        }
-        catch (IndexOutOfRangeException){
-            Debug.Log("No more puzzles to load");
-            GameObject.Find("GameManager").GetComponent<GameManager>().SetAreaFinished();
-            return;
-            //GameObject.Find("GameManager").GetComponent<GameManager>().CloseAllScenes();
-        }
     }
 }
