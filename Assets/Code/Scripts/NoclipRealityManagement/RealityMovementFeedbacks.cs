ddf7fa02a1b93942d559d0182991d8c3a65fb2ee
stefanofossati <10569836@mail.polimi.it>
2022-11-22 00:04:53 +0100

    change names, comment and set right parameters

--- Assets/Code/Scripts/NoclipRealityManagement/RealityMovementFeedbacks.cs
+++ Assets/Code/Scripts/NoclipRealityManagement/RealityMovementFeedbacks.cs
@@ -5,22 +5,25 @@ using UnityEngine;
 public class RealityMovementFeedbacks : MonoBehaviour
 {
     [Header("Headbob parameters")]
-    [SerializeField] private float _headbobVariationSprinting = 0.1f;
-    [SerializeField] private float _headbobVariationWalking = 0.05f;
-    [SerializeField] private float _headbobPeriodMultiplier = 2f;
+    [Tooltip("Indicates the variation of the headbob movement in walking mode, corresponds to the amplitude of the sine wave")]
+    [SerializeField] private float _headbobVariationSprinting = 0.06f;
+    [Tooltip("Indicates the variation of the headbob movement in sprinting mode, corresponds to the amplitude of the sine wave")]
+    [SerializeField] private float _headbobVariationWalking = 0.06f;
+    [Tooltip("It's the frequency of  the headbob movement, high value correspond to an high frequency")]
+    [SerializeField] private float _headbobFrequency = 3f;
     private float _headbobVariation;
     private float _headBobTimer;
 
     [Header("Footstep parameters")]
-    [SerializeField] private float _stepSpeed = 2f;
+    /*[Tooltip("Indicates the time of the reproduction of the sound, high value corresponds to slow speed")]
+    [SerializeField]*/ private float reproductionTime = 1f;
+    [Tooltip("Indicates the minimum speed of the player for activate the sound")]
     [SerializeField] private float _speedAudioActivation = 2f;
-    [SerializeField] private AudioSource _audio;
+    [SerializeField] private AudioSource _audioSource;
     [SerializeField] private AudioClip[] _footstepClips;
     [SerializeField] private AudioClip _landSound;
     private float _footstepTimer;
-
     private float _moveSpeed;
-
     private Vector3 _cameraPosition;
 
     [Space]
@@ -34,12 +37,6 @@ public class RealityMovementFeedbacks : MonoBehaviour
     {
         _realityMovementCalibration = GetComponent<RealityMovementCalibration>();
         _cameraPosition = _camera.transform.localPosition;
-    }
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
     }
 
     // Update is called once per frame
@@ -72,7 +69,7 @@ public class RealityMovementFeedbacks : MonoBehaviour
             if (_moveSpeed > 1f)
             {
                 // The time is incremented each time that the camera moves
-                _headBobTimer += Time.deltaTime * _realityMovementCalibration.GetMaxVelocity() * _headbobPeriodMultiplier;  // This speed changes related to the reality player state
+                _headBobTimer += Time.deltaTime * _realityMovementCalibration.GetMaxVelocity() * _headbobFrequency;  // This speed changes related to the reality player state
                 // The position of the camera change on the y axis in order to do an up and down. The maximum difference is managed by the multiplier
                 _camera.transform.localPosition = _cameraPosition + (new Vector3(0, Mathf.Sin(_headBobTimer), 0) * _headbobVariation);
             }
@@ -88,28 +85,29 @@ public class RealityMovementFeedbacks : MonoBehaviour
         if (_realityMovementCalibration.GetState() != RealityMovementCalibration.MovementState.Air)
         {
             _footstepTimer -= Time.deltaTime * _realityMovementCalibration.GetMaxVelocity();
-            if (_moveSpeed > _speedAudioActivation && _footstepTimer < 0 && _camera.transform.localPosition.y < _cameraPosition.y - _headbobVariation*0.9)
+            //checks the minimum speed of audio activation, if the audio is still playing and if the camera is at the low position of the headbob sine wave
+            if (_moveSpeed > _speedAudioActivation && _footstepTimer < 0 && _camera.transform.localPosition.y < _cameraPosition.y - _headbobVariation*0.9) 
             {
-                _audio.PlayOneShot(_footstepClips[Random.Range(0, _footstepClips.Length - 1)]);
-                _audio.volume = Random.Range(0.8f, 1);
-                _footstepTimer = _stepSpeed;
+                _audioSource.PlayOneShot(_footstepClips[Random.Range(0, _footstepClips.Length - 1)]);
+                _audioSource.volume = Random.Range(0.8f, 1);
+                _footstepTimer = reproductionTime;
             }
             
             // if the player go from a state of air to a state of ground. so this is the sound when the player land on the ground
             if (_lastState == RealityMovementCalibration.MovementState.Air) 
             {
-                _audio.PlayOneShot(_landSound); 
-                _audio.volume = 1.5f;
+                _audioSource.PlayOneShot(_landSound); 
+                _audioSource.volume = 1.5f;
             }
             
         }
         else // if the reality player is in air
         {
-            _audio.Stop();
-            _footstepTimer = _stepSpeed;
+            _audioSource.Stop();
+            _footstepTimer = reproductionTime;
         }
         
-        _lastState = _realityMovementCalibration.GetState();
+        _lastState = _realityMovementCalibration.GetState(); // Saves the last state of the player because it is used for the land sound
         
         
     }
