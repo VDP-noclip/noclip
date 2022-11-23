cfc2c28a65a62f65a3e4256d0704be9d96426e1a
bottolo <64470114+bottolo@users.noreply.github.com>
2022-11-23 12:14:11 +0100

    Gameplay settings redesigned, sliders working

--- Assets/Code/Scripts/MenuManagement/PauseMenuController.cs
+++ Assets/Code/Scripts/MenuManagement/PauseMenuController.cs
@@ -71,9 +71,6 @@ public class PauseMenuController : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
 
-
-        
-        
         pauseMenuUI.SetActive(false);
         audioMenuUI.SetActive(false);
         settingsMenuUI.SetActive(false);
