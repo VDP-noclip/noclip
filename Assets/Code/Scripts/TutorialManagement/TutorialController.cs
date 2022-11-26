f10bacfdb4b6daa01c8077685bbdea631a24eb8b
spig95 <nicolaischia95@gmail.com>
2022-11-26 14:31:43 +0100

    Improve noclip manager and tutorial text

--- Assets/Code/Scripts/TutorialManagement/TutorialController.cs
+++ Assets/Code/Scripts/TutorialManagement/TutorialController.cs
@@ -1,57 +1,57 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using POLIMIGameCollective;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class TutorialController : MonoBehaviour
+namespace Code.Scripts.TutorialManagement
 {
-    [SerializeField] private GameObject _controlsContainer;
-    [SerializeField] private TMP_Text _tutorialText;
-    [SerializeField] private float _hintDuration;
-    private float _endHintTime;
-
-    private void Awake()
+    public class TutorialController : MonoBehaviour
     {
-        EventManager.StartListening ("PressP", PressP);
-        EventManager.StartListening ("DisplayHint", DisplayHint);
-    }
+        [SerializeField] private GameObject _controlsContainer;
+        [SerializeField] private TMP_Text _tutorialText;
+        [SerializeField] private float _hintDuration = 4f;
+        private float _endHintTime;
 
-
-    private void PressP()
-    {
-        EventManager.StopListening("PressP", PressP);
-        StartCoroutine(DisplayHintCoroutine("PRESS P"));
-        EventManager.StartListening("PressP", PressP);
-    }
-
-    private void DisplayHint(string hint)
-    {
-        
-        EventManager.StopListening("DisplayHint", DisplayHint);
-        StartCoroutine(DisplayHintCoroutine(hint));
-        EventManager.StartListening("DisplayHint", DisplayHint);
-        
-    }
-
-    private IEnumerator DisplayHintCoroutine(string hint)
-    {
-        _controlsContainer.SetActive(true);
-        _tutorialText.text = hint;
-
-        _endHintTime = Time.time + 4;
-
-        while (Time.time < _endHintTime)
+        private void Awake()
         {
-            yield return new WaitForSecondsRealtime(0.5f);
+            EventManager.StartListening ("ClearHints", ClearHints);
+            EventManager.StartListening ("DisplayHint", DisplayHint);
+            _tutorialText.text = "";
         }
 
-        _controlsContainer.SetActive(false);
 
-        yield return null;
+        private void ClearHints()
+        {
+            EventManager.StopListening("ClearHints", ClearHints);
+            _tutorialText.text = "";
+            EventManager.StartListening("ClearHints", ClearHints);
+        }
+
+        private void DisplayHint(string hint)
+        {
+        
+            EventManager.StopListening("DisplayHint", DisplayHint);
+            StartCoroutine(DisplayHintCoroutine(hint));
+            EventManager.StartListening("DisplayHint", DisplayHint);
+        
+        }
+
+        private IEnumerator DisplayHintCoroutine(string hint)
+        {
+            _controlsContainer.SetActive(true);
+            _tutorialText.text = hint;
+
+            _endHintTime = Time.time + _hintDuration;
+
+            while (Time.time < _endHintTime)
+            {
+                yield return new WaitForSecondsRealtime(0.5f);
+            }
+
+            _controlsContainer.SetActive(false);
+
+            yield return null;
+        }
+
     }
-
 }
