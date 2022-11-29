6edc8008d3379d5c85f78060d68d629bad53ae79
stefanofossati <10569836@mail.polimi.it>
2022-11-29 23:30:47 +0100

    correct input fields, create prefabs, add multiple sub forms

--- Assets/Code/Scripts/Feedbacks/MenuManager.cs
+++ Assets/Code/Scripts/Feedbacks/MenuManager.cs
@@ -9,7 +9,6 @@ public class MenuManager : Singleton<MenuManager>
 {
     public enum Menu
     {
-        Main,
         Feedback0,
         Feedback1,
         Feedback2,
@@ -17,28 +16,23 @@ public class MenuManager : Singleton<MenuManager>
         Feedback4,
     };
     
-    public GameObject MainMenu;
-    public GameObject FeedbackMenu0;
-    public GameObject FeedbackMenu1;
-    public GameObject FeedbackMenu2;
-    public GameObject FeedbackMenu3;
-    public GameObject FeedbackMenu4;
-    public GameObject FeedbackContainer;
+    [SerializeField] private GameObject FeedbackMenu0;
+    [SerializeField] private GameObject FeedbackMenu1;
+    [SerializeField] private GameObject FeedbackMenu2;
+    [SerializeField] private GameObject FeedbackMenu3;
+    [SerializeField] private GameObject FeedbackMenu4;
 
     private void SetMenu(Menu menu)
     {
+        
         FeedbackMenu0.SetActive(false);
         FeedbackMenu1.SetActive(false);
         FeedbackMenu2.SetActive(false);
         FeedbackMenu3.SetActive(false);
         FeedbackMenu4.SetActive(false);
-        FeedbackContainer.SetActive(true);
 
         switch (menu)
         {
-            case Menu.Main:
-                MainMenu.SetActive(true);
-                break;
             case Menu.Feedback0:
                 FeedbackMenu0.SetActive(true);
                 break;
@@ -58,15 +52,8 @@ public class MenuManager : Singleton<MenuManager>
         }
     }
 
-    public void OpenMainMenu()
-    {
-        SetMenu(Menu.Main);
-        FeedbackContainer.SetActive(false);
-    }
-
     public void OpenFeedbackMenu0()
     {
-        FeedbackContainer.SetActive(true);
         SetMenu(Menu.Feedback0);
     }
     
@@ -93,6 +80,6 @@ public class MenuManager : Singleton<MenuManager>
     public void Submit()
     {
         Debug.Log("Submitting Feedback");
-        SetMenu(Menu.Main);
+        SetMenu(Menu.Feedback0);
     }
 }
--- Assets/Code/Scripts/Feedbacks/SendToGoogle.cs
+++ Assets/Code/Scripts/Feedbacks/SendToGoogle.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
@@ -71,31 +74,34 @@ public class SendToGoogle : MonoBehaviour
     
     [SerializeField] private VideoGamesName VideogameName;
 
-    [Space]
-    
-    [Header("Feedback0")]
-    [SerializeField] private GameObject[] _multipleChoices0_0;
-    [SerializeField] private GameObject[] _multipleChoices0_1;
-    [SerializeField] private Button _nextPage0_0;
+    [Space] [Header("Feedback0")] 
+    [SerializeField] private Transform _multipleChoices0_0;
+    [SerializeField] private Transform _multipleChoices0_1;
+    private GameObject[] _multipleChoicesArray0_0;
+    private GameObject[] _multipleChoicesArray0_1;
+    [SerializeField] [NotNull]private GameObject _buttonNext0;
     private string _multipleChoice0_0 = "null";
     private string _multipleChoice0_1 = "null";
 
     [Space]
     
     [Header("Feedback1")]
-    [SerializeField] private GameObject[] _multipleChoices1_0;
-    [SerializeField] private InputField _suggestions1_0;
-    [SerializeField] private Button _nextPage1_0;
+    [SerializeField] private Transform _multipleChoices1_0;
+    private GameObject[] _multipleChoicesArray1_0;
+    [SerializeField] private TMP_InputField _suggestions1_0;
+    [SerializeField] [NotNull] private GameObject _buttonNext1;
     private string _multipleChoice1_0 = "null";
     private string _suggestion1_0 = "null"; 
     
     [Space]
     
     [Header("Feedback2")]
-    [SerializeField] private GameObject[] _multipleChoices2_0;
-    [SerializeField] private GameObject[] _multipleChoices2_1;
-    [SerializeField] private InputField _suggestions2_0;
-    [SerializeField] private Button _nextPage2_0;
+    [SerializeField] private Transform _multipleChoices2_0;
+    [SerializeField] private Transform _multipleChoices2_1;
+    private GameObject[] _multipleChoicesArray2_0;
+    private GameObject[] _multipleChoicesArray2_1;
+    [SerializeField] private TMP_InputField _suggestions2_0;
+    [SerializeField] [NotNull] private GameObject _buttonNext2;
     private string _multipleChoice2_0 = "null";
     private string _multipleChoice2_1 = "null";
     private string _suggestion2_0 = "null";
@@ -103,17 +109,18 @@ public class SendToGoogle : MonoBehaviour
     [Space]
     
     [Header("Feedback3")]
-    [SerializeField] private GameObject[] _multipleChoices3_0;
-    [SerializeField] private InputField _suggestions3_0;
-    [SerializeField] private Button _nextPage3_0;
+    [SerializeField] private Transform _multipleChoices3_0;
+    private GameObject[] _multipleChoicesArray3_0;
+    [SerializeField] private TMP_InputField _suggestions3_0;
+    [SerializeField] [NotNull] private GameObject _buttonNext3;
     private string _multipleChoice3_0 = "null";
     private string _suggestion3_0 = "null";
     
     [Space]
     
     [Header("Feedback4")]
-    [SerializeField] private InputField _suggestions4_0;
-    [SerializeField] private Button _nextPage4_0;
+    [SerializeField] private TMP_InputField _suggestions4_0;
+    [SerializeField] [NotNull] private GameObject _buttonNext4;
     private string _multipleChoice4_0 = "null";
     private string _suggestion4_0 = "null";
 
@@ -121,42 +128,65 @@ public class SendToGoogle : MonoBehaviour
     private void Awake()
     {
         // Page0
-        foreach (GameObject gameObject in _multipleChoices0_0)
+        _multipleChoicesArray0_0 = retriveMultipleChoices(_multipleChoices0_0);
+        _multipleChoicesArray0_1 = retriveMultipleChoices(_multipleChoices0_1);
+
+        foreach (GameObject var in _multipleChoicesArray0_0)
         {
-            gameObject.GetComponent<Button>().onClick.AddListener(delegate {_multipleChoice0_0 = SaveChoice(gameObject.name, _multipleChoices0_0); });
+            var.GetComponent<Button>().onClick.AddListener(delegate {_multipleChoice0_0 = SaveChoice(var.name, _multipleChoicesArray0_0); });
         }
         
-        foreach (GameObject gameObject in _multipleChoices0_1)
+        foreach (GameObject var in _multipleChoicesArray0_1)
         {
-            gameObject.GetComponent<Button>().onClick.AddListener(delegate {_multipleChoice0_1 = SaveChoice(gameObject.name, _multipleChoices0_1 ); });
+            var.GetComponent<Button>().onClick.AddListener(delegate {_multipleChoice0_1 = SaveChoice(var.name, _multipleChoicesArray0_1 ); });
         }
+        
+        _buttonNext0.GetComponent<Button>().onClick.AddListener(SendToGoogleFeedback0);
         
         //Page1
-        foreach (GameObject gameObject in _multipleChoices1_0)
+        _multipleChoicesArray1_0 = retriveMultipleChoices(_multipleChoices1_0);
+        
+        foreach (GameObject var in _multipleChoicesArray1_0)
         {
-            gameObject.GetComponent<Button>().onClick.AddListener(delegate {_multipleChoice1_0 = SaveChoice(gameObject.name, _multipleChoices1_0 ); });
+            var.GetComponent<Button>().onClick.AddListener(delegate {_multipleChoice1_0 = SaveChoice(var.name, _multipleChoicesArray1_0 ); });
         }
         
+        _buttonNext1.GetComponent<Button>().onClick.AddListener(SaveSuggestion1);
+        _buttonNext1.GetComponent<Button>().onClick.AddListener(SendToGoogleFeedback1);
+       
         
         //Page2
-        foreach (GameObject gameObject in _multipleChoices2_0)
+        _multipleChoicesArray2_0 = retriveMultipleChoices(_multipleChoices2_0);
+        _multipleChoicesArray2_1 = retriveMultipleChoices(_multipleChoices2_1);
+        
+        foreach (GameObject var in _multipleChoicesArray2_0)
         {
-            gameObject.GetComponent<Button>().onClick.AddListener(delegate {_multipleChoice2_0 = SaveChoice(gameObject.name, _multipleChoices2_0 ); });
+            var.GetComponent<Button>().onClick.AddListener(delegate {_multipleChoice2_0 = SaveChoice(var.name, _multipleChoicesArray2_0 ); });
         }
         
-        foreach (GameObject gameObject in _multipleChoices2_1)
+        foreach (GameObject var in _multipleChoicesArray2_1)
         {
-            gameObject.GetComponent<Button>().onClick.AddListener(delegate {_multipleChoice2_1 = SaveChoice(gameObject.name, _multipleChoices2_1 ); });
+            var.GetComponent<Button>().onClick.AddListener(delegate {_multipleChoice2_1 = SaveChoice(var.name, _multipleChoicesArray2_1 ); });
         }
         
+        _buttonNext2.GetComponent<Button>().onClick.AddListener(SaveSuggestion2);
+        _buttonNext2.GetComponent<Button>().onClick.AddListener(SendToGoogleFeedback2);
         
         //Page3
-        foreach (GameObject gameObject in _multipleChoices3_0)
+        _multipleChoicesArray3_0 = retriveMultipleChoices(_multipleChoices3_0);
+        foreach (GameObject var in _multipleChoicesArray3_0)
         {
-            gameObject.GetComponent<Button>().onClick.AddListener(delegate {_multipleChoice3_0 = SaveChoice(gameObject.name, _multipleChoices3_0); });
+            var.GetComponent<Button>().onClick.AddListener(delegate {_multipleChoice3_0 = SaveChoice(var.name, _multipleChoicesArray3_0); });
         }
         
-
+        _buttonNext3.GetComponent<Button>().onClick.AddListener(SaveSuggestion3);
+        _buttonNext3.GetComponent<Button>().onClick.AddListener(SendToGoogleFeedback3);
+        
+        
+        //Page4
+        
+        _buttonNext4.GetComponent<Button>().onClick.AddListener(SaveSuggestion4);
+        _buttonNext4.GetComponent<Button>().onClick.AddListener(SendToGoogleFeedback4);
     }
 
     public void SendFeedback()
@@ -217,8 +247,6 @@ public class SendToGoogle : MonoBehaviour
             Debug.Log("Form upload complete!");
         }
         
-        // at the end go back to the main menu
-        MenuManager.Instance.OpenMainMenu();
     }
     IEnumerator PostFeedback(string videogame_name, string feedback) 
     {
@@ -246,15 +274,162 @@ public class SendToGoogle : MonoBehaviour
         {
             Debug.Log("Form upload complete!");
         }
+    }
+
+    public void SendToGoogleFeedback0()
+    {
+        StartCoroutine(SendToGoogleFeedback0Coroutine());
+    }
+    
+    public void SendToGoogleFeedback1()
+    {
+        StartCoroutine(SendToGoogleFeedback1Coroutine());
+    }
+    public void SendToGoogleFeedback2()
+    {
+        StartCoroutine(SendToGoogleFeedback2Coroutine());
+    }
+    public void SendToGoogleFeedback3()
+    {
+        StartCoroutine(SendToGoogleFeedback3Coroutine());
+    }
+    public void SendToGoogleFeedback4()
+    {
+        StartCoroutine(SendToGoogleFeedback4Coroutine());
+    }
+    
+    private IEnumerator SendToGoogleFeedback0Coroutine()
+    {
+        string URL = "https://docs.google.com/forms/d/e/1FAIpQLSef5l-ZUoHC7c_VLqy09NA2VTbu1VCwNw7NZLjB8mOI2iipUA/formResponse";
         
-        // at the end go back to the main menu
-        MenuManager.Instance.OpenMainMenu();
+        WWWForm form = new WWWForm();
+
+        form.AddField("entry.643657764", _multipleChoice0_0);
+        form.AddField("entry.1301141610", _multipleChoice0_1);
+        form.AddField("entry.235040056", "chackpoint");
+
+        UnityWebRequest www = UnityWebRequest.Post(URL, form);
+
+        yield return www.SendWebRequest();
+
+        print(www.error);
+        
+        if (www.isNetworkError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log("Form upload complete!");
+        }
+    }
+    
+    private IEnumerator SendToGoogleFeedback1Coroutine()
+    {
+        string URL = "https://docs.google.com/forms/d/e/1FAIpQLSfY6mCvNvumzwpGpiYJAbaeqnomloZ_hs5NXIG9f1EolvsJ4g/formResponse";
+        
+        WWWForm form = new WWWForm();
+
+        form.AddField("entry.1689264014", _multipleChoice1_0);
+        form.AddField("entry.1795851765", _suggestion1_0);
+        form.AddField("entry.2089839631", "chackpoint");
+
+        UnityWebRequest www = UnityWebRequest.Post(URL, form);
+
+        yield return www.SendWebRequest();
+
+        print(www.error);
+        
+        if (www.isNetworkError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log("Form upload complete!");
+        }
+    }
+    private IEnumerator SendToGoogleFeedback2Coroutine()
+    {
+        string URL = "https://docs.google.com/forms/d/e/1FAIpQLSccb7D7IJbgEmdUOC8XBd1oQH3VmwDGbFygk7C4qQETN2eBHA/formResponse";
+        
+        WWWForm form = new WWWForm();
+
+        form.AddField("entry.2064137350", _multipleChoice2_0);
+        form.AddField("entry.608431837", _multipleChoice2_1);
+        form.AddField("entry.122820910", _suggestion2_0);
+        form.AddField("entry.969916236", "chackpoint");
+
+        UnityWebRequest www = UnityWebRequest.Post(URL, form);
+
+        yield return www.SendWebRequest();
+
+        print(www.error);
+        
+        if (www.isNetworkError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log("Form upload complete!");
+        }
+    }
+    
+    private IEnumerator SendToGoogleFeedback3Coroutine()
+    {
+        string URL = "https://docs.google.com/forms/d/e/1FAIpQLSeTdMlbw04t4Dpq-FFakdZd9FWxaDCraGnGCy-tYT5EDhmXaw/formResponse";
+        
+        WWWForm form = new WWWForm();
+
+        form.AddField("entry.2031543232", _multipleChoice3_0);
+        form.AddField("entry.1198441079", _suggestion3_0);
+        form.AddField("entry.1958101121", "chackpoint");
+
+        UnityWebRequest www = UnityWebRequest.Post(URL, form);
+
+        yield return www.SendWebRequest();
+
+        print(www.error);
+        
+        if (www.isNetworkError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log("Form upload complete!");
+        }
+    }
+    
+    private IEnumerator SendToGoogleFeedback4Coroutine()
+    {
+        string URL = "https://docs.google.com/forms/d/e/1FAIpQLSfL8axq46a4vJCcQ3L6dCv8VasmyL26Xd9wkJ963MYld-7COw/formResponse";
+        
+        WWWForm form = new WWWForm();
+
+        form.AddField("entry.2103217825", _suggestion4_0);
+        form.AddField("entry.1185853159", "chackpoint");
+
+        UnityWebRequest www = UnityWebRequest.Post(URL, form);
+
+        yield return www.SendWebRequest();
+
+        print(www.error);
+        
+        if (www.isNetworkError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log("Form upload complete!");
+        }
     }
 
     public string SaveChoice(string choice, GameObject[] list)
     {
-        
-        
+
         foreach (GameObject button in list)
         {
             if (button.name != choice)
@@ -266,7 +441,8 @@ public class SendToGoogle : MonoBehaviour
                 button.GetComponent<Button>().image.color = Color.cyan;
             }
         }
-
+        
+        Debug.Log(choice);
         return choice;
     }
 
@@ -288,5 +464,19 @@ public class SendToGoogle : MonoBehaviour
     public void SaveSuggestion4()
     {
         _suggestion4_0 = _suggestions4_0.text;
+    }
+
+    private GameObject[] retriveMultipleChoices(Transform trans)
+    {
+        List<GameObject> list = new();
+        foreach (Transform child in trans)
+        {
+            if (child.name != "Question")
+            {
+                list.Add(child.gameObject);
+            }
+        }
+        Debug.Log(list.ToArray()[2].name);
+        return list.ToArray();
     }
 }
