c4a7800cfd8c4cb438fa6e64678604e000f3ef98
TizioMaurizio <maurizio.vetere@mail.polimi.it>
2022-11-30 11:41:20 +0100

    spawn the player in the direction pointed by spawnplatform's arrow

--- Assets/Code/Scripts/LevelManagement/SpawnPlayer.cs
+++ Assets/Code/Scripts/LevelManagement/SpawnPlayer.cs
@@ -31,10 +31,13 @@ public class SpawnPlayer : MonoBehaviour
                 if(_puzzleReached){
                     GameObject player = GameObject.Find("AllPlayer").gameObject;
                     //move the player to the spawn point plus 1 meter
-                    player.transform.position = transform.position + new Vector3(0, 20, 0);
+                    player.transform.position = transform.position + new Vector3(0, 5, 0);
                     //rotate the player to the spawn point
                     player.transform.rotation = transform.rotation;
-                    //disable this game object
+                    //update mouselook
+                    player.GetComponentInChildren<MouseLook>().SyncYRotation();
+                    //disable this game component
+                    this.enabled = false;
                     gameObject.SetActive(false);
                 }
                 else{
--- Assets/Code/Scripts/PlayerManagement/MouseLook.cs
+++ Assets/Code/Scripts/PlayerManagement/MouseLook.cs
@@ -57,6 +57,7 @@ public class MouseLook : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         _prevTransformRotation = _transform.rotation;
+        _yRotation = _orientation.eulerAngles.y;
     }
     
     void Update()
@@ -166,6 +167,12 @@ public class MouseLook : MonoBehaviour
         // IMPORTANT: Don't change the order of the two following lines: if _orientation and _transform are the same gameobject it doesn't work due to value override.
         _orientation.rotation = Quaternion.Euler(0, _yRotation, 0);  // updates the orientation of the gameobject that has the orientation information of the camera on the y axis
         _transform.rotation = Quaternion.Euler(_xRotation*_invertY, _yRotation, 0);  // updates the camera orientation
+        _prevTransformRotation = _transform.rotation;
+    }
+
+    public void SyncYRotation()
+    {
+        _yRotation = _orientation.eulerAngles.y;
         _prevTransformRotation = _transform.rotation;
     }
 }
