bf07bd41de740cbc1543994e83a66dc4723cf22e
bottolo <64470114+bottolo@users.noreply.github.com>
2022-12-02 22:18:31 +0100

    Inert Y doesn't work, menu fixed though

--- Assets/Code/Scripts/MenuManagement/PauseMenuController.cs
+++ Assets/Code/Scripts/MenuManagement/PauseMenuController.cs
@@ -75,7 +75,7 @@ public class PauseMenuController : MonoBehaviour
     }
     private void Update()
     {
-        if (Input.GetButtonDown("Pause"))
+        if (Input.GetKeyDown("1"))
         {
             if (!_isPaused)
             {
