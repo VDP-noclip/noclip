afb5dbfa263fcbf353721bda635fffbcdf2e422a
stefanofossati <10569836@mail.polimi.it>
2022-12-03 12:29:12 +0100

    add vsync line

--- Assets/Code/Scripts/LevelManagement/GameManager.cs
+++ Assets/Code/Scripts/LevelManagement/GameManager.cs
@@ -35,6 +35,7 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         Application.targetFrameRate = 60;
+        QualitySettings.vSyncCount = 1;
     }
 
     private void Update()
