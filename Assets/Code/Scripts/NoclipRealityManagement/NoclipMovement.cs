1ab878817039ed520ef85390e64b675cf5a15612
TizioMaurizio <maurizio.vetere@mail.polimi.it>
2022-12-12 14:32:39 +0100

    smooth noclip movement, change ctrl and space movement to face direction relative, combining absolute and relative is confusing imho

--- Assets/Code/Scripts/NoclipRealityManagement/NoclipMovement.cs
+++ Assets/Code/Scripts/NoclipRealityManagement/NoclipMovement.cs
@@ -16,14 +16,28 @@ public class NoclipMovement : MonoBehaviour
     [Tooltip("Camera movement by 'W','A','S','D','Q','E' keys is active")]
     private bool _enableMovement = true;
 
-    [SerializeField]
+    [Header("Base")]
     [Tooltip("Camera movement speed")]
-    private float _movementSpeed = 10f;
+    [SerializeField] private float _baseSpeed = 50f;
+    [SerializeField] private float _baseAcceleration = 100f;
     
+    private float _maxSpeed = 50f;
+    private float _acceleration = 50f;
+
+    [Header("Boost")]
     [SerializeField]
     [Tooltip("Speed of the quick camera movement when holding the 'Left Shift' key")]
-    private float _boostedSpeed = 50f;
+    private float _boostedSpeed = 100f;
+    [SerializeField] private float _boostAcceleration = 200f;
+
+    [Header("Smooth brake")]
+    [Tooltip("MaxSpeed percentage where braking deceleration becomes nonlinear (smoother braking close to zero)")]
+    [SerializeField] private float _smoothBrakeRange = 0.1f;
+    [Tooltip("Higher is smoother")]
+    [SerializeField] private float _smoothBrakeFactor = 0.9f;
     
+    /*
+    [Header("Controls")]
     [SerializeField]
     [Tooltip("Boost speed")]
     private KeyCode _boostSpeed = KeyCode.LeftShift;
@@ -35,7 +49,7 @@ public class NoclipMovement : MonoBehaviour
     [SerializeField]
     [Tooltip("Move down")]
     private KeyCode _moveDown = KeyCode.Q;
-
+    */
     private Transform _transform;
     private CameraManager _cameraManager;
     private Transform _noclipCamera;
@@ -47,7 +61,10 @@ public class NoclipMovement : MonoBehaviour
     
     private float _horizontalInput;
     private float _verticalInput;
+    private float _upDownInput;
 
+    private Vector3 _speed = Vector3.zero;
+    
     private bool _insideRealityPlayer = true;
     
     private void Awake()
@@ -55,7 +72,6 @@ public class NoclipMovement : MonoBehaviour
         _transform = GetComponent<Transform>();
         _noclipManager = FindObjectOfType<NoclipManager>();
         _noclipCamera = GetComponent<Transform>();
-
     }
 
     // Update is called once per frame
@@ -66,39 +82,79 @@ public class NoclipMovement : MonoBehaviour
 
         if (_enableMovement && _noclipManager.IsNoclipEnabled())
         {
-            Vector3 deltaPosition = Vector3.zero;
-            float currentSpeed = _movementSpeed;
-            
             _horizontalInput = Input.GetAxisRaw("Horizontal");
             _verticalInput = Input.GetAxisRaw("Vertical");
+            bool moveUp = Input.GetButton("NoclipMoveUp");
+            bool moveDown = Input.GetButton("NoclipMoveDown");
+            bool boost = Input.GetButton("BoostNoclip");
+            _upDownInput = 0;
+            if(moveUp){
+                _upDownInput += 1;
+            }
+            if(moveDown){
+                _upDownInput += -1;
+            }
 
-            deltaPosition += _transform.forward * _verticalInput + _transform.right * _horizontalInput;
+            if (boost)
+            {
+                _maxSpeed = _boostedSpeed;
+                _acceleration = _boostAcceleration;
+            }
+            else
+            {
+                _maxSpeed = _baseSpeed;
+                _acceleration = _baseAcceleration;
+            }
 
             
-            if (Input.GetButton("BoostNoclip"))
-                currentSpeed = _boostedSpeed;
-               
-            /*if (Input.GetKey(KeyCode.W))
-                deltaPosition += _transform.forward;
+            //_horizontalInput and _verticalInput move with linear acceleration in the direction of the camera
+            if (_horizontalInput != 0 || _verticalInput != 0 || _upDownInput != 0)
+            {
+                //increase speed in direction of camera with acceleration
+                _speed += (_transform.forward * _verticalInput).normalized * _acceleration * Time.deltaTime;
+                _speed += (_transform.right * _horizontalInput).normalized * _acceleration * Time.deltaTime;
+                _speed += (_transform.up * _upDownInput).normalized * _acceleration * Time.deltaTime;
+                //limit speed
+                _speed = Vector3.ClampMagnitude(_speed, _maxSpeed);
+            }
+            else
+            {
+                Vector3 deltaSpeed = _speed.normalized * _acceleration * Time.deltaTime;
+                if(_speed.magnitude > deltaSpeed.magnitude && _speed.magnitude > _maxSpeed * _smoothBrakeRange)
+                {
+                    _speed -= deltaSpeed;
+                    _smoothBrake = false;
+                }
+                else
+                {
+                    _smoothBrake = true;
+                }
+            }
+            //move in direction of speed
+            transform.position += _speed * Time.deltaTime;
+            /*
+            if (_horizontalInput != 0 || _verticalInput != 0){
+                //speed lerp
+                _speed = Vector3.Lerp(_speed, (_transform.forward * _verticalInput + _transform.right * _horizontalInput).normalized * _maxSpeed, _acceleration * Time.deltaTime);
+            }
+            else
+            {
+                _speed = Vector3.Lerp(_speed, Vector3.zero, _acceleration * Time.deltaTime);
+            }
+            transform.position += _speed * Time.deltaTime;
+            */
+        }
+        else
+        {
+            _speed = Vector3.zero;
+        }
+    }
 
-            if (Input.GetKey(KeyCode.S))
-                deltaPosition -= _transform.forward;
+    private bool _smoothBrake = false;
 
-            if (Input.GetKey(KeyCode.A))
-                deltaPosition -= _transform.right;
-
-            if (Input.GetKey(KeyCode.D))
-                deltaPosition += _transform.right;*/
-
-            if (Input.GetButton("NoclipMoveUp"))
-                deltaPosition += Vector3.up;
-
-            if (Input.GetButton("NoclipMoveDown"))
-                deltaPosition -= Vector3.up;
-            
-            
-            _transform.position += deltaPosition * (currentSpeed * Time.deltaTime);
-            
+    private void FixedUpdate(){
+        if(_smoothBrake){
+            _speed *= _smoothBrakeFactor;
         }
     }
 
