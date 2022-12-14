dfb79c48bd653e1160e4ad73db4ea7677b17b33c
TizioMaurizio <48728364+TizioMaurizio@users.noreply.github.com>
2022-12-14 13:36:53 +0100

    Smooth noclip movement (#193)
    smooth noclip movement with inspector serialized parameters, changed
    ctrl and space movement to face direction relative, combining absolute
    and relative is confusing imho

--- Assets/Code/Scripts/NoclipRealityManagement/NoclipMovement.cs
+++ Assets/Code/Scripts/NoclipRealityManagement/NoclipMovement.cs
@@ -16,14 +16,28 @@ public class NoclipMovement : MonoBehaviour
     [Tooltip("Camera movement by 'W','A','S','D','Q','E' keys is active")]
     private bool _enableMovement = true;
 
-    [SerializeField]
+    [Header("Base")]
     [Tooltip("Camera movement speed")]
-    private float _movementSpeed = 10f;
+    [SerializeField] private float _baseSpeed = 1f;
+    [SerializeField] private float _baseAcceleration = 0.05f;
     
+    private float _maxSpeed = 1f;
+    private float _acceleration = 0.05f;
+
+    [Header("Boost")]
     [SerializeField]
     [Tooltip("Speed of the quick camera movement when holding the 'Left Shift' key")]
-    private float _boostedSpeed = 50f;
+    private float _boostedSpeed = 2f;
+    [SerializeField] private float _boostAcceleration = 0.1f;
+
+    [Header("Smooth brake")]
+    [Tooltip("MaxSpeed percentage where braking deceleration becomes nonlinear (smoother braking close to zero)")]
+    [SerializeField] private float _smoothBrakeRange = 0.1f;
+    [Tooltip("Higher is smoother")]
+    [SerializeField] private float _smoothBrakeFactor = 0.93f;
     
+    /*
+    [Header("Controls")]
     [SerializeField]
     [Tooltip("Boost speed")]
     private KeyCode _boostSpeed = KeyCode.LeftShift;
@@ -35,7 +49,7 @@ public class NoclipMovement : MonoBehaviour
     [SerializeField]
     [Tooltip("Move down")]
     private KeyCode _moveDown = KeyCode.Q;
-
+    */
     private Transform _transform;
     private CameraManager _cameraManager;
     private Transform _noclipCamera;
@@ -47,7 +61,10 @@ public class NoclipMovement : MonoBehaviour
     
     private float _horizontalInput;
     private float _verticalInput;
+    private float _upDownInput;
 
+    private Vector3 _speed = Vector3.zero;
+    
     private bool _insideRealityPlayer = true;
     
     private void Awake()
@@ -55,52 +72,170 @@ public class NoclipMovement : MonoBehaviour
         _transform = GetComponent<Transform>();
         _noclipManager = FindObjectOfType<NoclipManager>();
         _noclipCamera = GetComponent<Transform>();
-
     }
 
     // Update is called once per frame
-    void Update()
+    /*void Update()
     {
         if (!_active)
             return;
 
         if (_enableMovement && _noclipManager.IsNoclipEnabled())
         {
-            Vector3 deltaPosition = Vector3.zero;
-            float currentSpeed = _movementSpeed;
-            
             _horizontalInput = Input.GetAxisRaw("Horizontal");
             _verticalInput = Input.GetAxisRaw("Vertical");
+            bool moveUp = Input.GetButton("NoclipMoveUp");
+            bool moveDown = Input.GetButton("NoclipMoveDown");
+            bool boost = Input.GetButton("BoostNoclip");
+            _upDownInput = 0;
+            if(moveUp){
+                _upDownInput += 1;
+            }
+            if(moveDown){
+                _upDownInput += -1;
+            }
 
-            deltaPosition += _transform.forward * _verticalInput + _transform.right * _horizontalInput;
+            if (boost)
+            {
+                _maxSpeed = _boostedSpeed;
+                _acceleration = _boostAcceleration;
+            }
+            else
+            {
+                _maxSpeed = _baseSpeed;
+                _acceleration = _baseAcceleration;
+            }
 
             
-            if (Input.GetButton("BoostNoclip"))
-                currentSpeed = _boostedSpeed;
-               
-            /*if (Input.GetKey(KeyCode.W))
-                deltaPosition += _transform.forward;
-
-            if (Input.GetKey(KeyCode.S))
-                deltaPosition -= _transform.forward;
-
-            if (Input.GetKey(KeyCode.A))
-                deltaPosition -= _transform.right;
-
-            if (Input.GetKey(KeyCode.D))
-                deltaPosition += _transform.right;*/
-
-            if (Input.GetButton("NoclipMoveUp"))
-                deltaPosition += Vector3.up;
-
-            if (Input.GetButton("NoclipMoveDown"))
-                deltaPosition -= Vector3.up;
-            
-            
-            _transform.position += deltaPosition * (currentSpeed * Time.deltaTime);
-            
+            //_horizontalInput and _verticalInput move with linear acceleration in the direction of the camera
+            if (_horizontalInput != 0 || _verticalInput != 0 || _upDownInput != 0)
+            {
+                //increase speed in direction of camera with acceleration
+                _speed += (_transform.forward * _verticalInput).normalized * _acceleration * Time.deltaTime;
+                _speed += (_transform.right * _horizontalInput).normalized * _acceleration * Time.deltaTime;
+                _speed += (_transform.up * _upDownInput).normalized * _acceleration * Time.deltaTime;
+                //limit speed
+                _speed = _speed.normalized * Mathf.Min(_speed.magnitude, _maxSpeed);
+            }
+            else
+            {
+                Vector3 deltaSpeed = _speed.normalized * _acceleration * Time.deltaTime;
+                if(_speed.magnitude > deltaSpeed.magnitude && _speed.magnitude > _maxSpeed * _smoothBrakeRange)
+                {
+                    _speed -= deltaSpeed;
+                    _smoothBrake = false;
+                }
+                else
+                {
+                    _smoothBrake = true;
+                }
+            }
+            //move in direction of speed
+            transform.position += _speed * Time.deltaTime;
+        }
+        else
+        {
+            _speed = Vector3.zero;
         }
     }
+
+    private bool _smoothBrake = false;
+
+    private void FixedUpdate(){
+        if(_smoothBrake){
+            _speed *= _smoothBrakeFactor;
+        }
+    }*/
+
+void Update()
+    {
+        //max between fixeddeltatime and (Time.deltaTime / Time.fixedDeltaTime)
+        float timeCorrection = Mathf.Min(1f, Time.deltaTime / Time.fixedDeltaTime);
+        //log timeCorrection
+        //Debug.Log(timeCorrection);
+        if (!_active)
+            return;
+
+        if (_enableMovement && _noclipManager.IsNoclipEnabled())
+        {
+            _horizontalInput = Input.GetAxisRaw("Horizontal");
+            _verticalInput = Input.GetAxisRaw("Vertical");
+            bool moveUp = Input.GetButton("NoclipMoveUp");
+            bool moveDown = Input.GetButton("NoclipMoveDown");
+            bool boost = Input.GetButton("BoostNoclip");
+            _upDownInput = 0;
+            if(moveUp){
+                _upDownInput += 1;
+            }
+            if(moveDown){
+                _upDownInput += -1;
+            }
+
+            if (boost)
+            {
+                _maxSpeed = _boostedSpeed;
+                _acceleration = _boostAcceleration;
+            }
+            else
+            {
+                _maxSpeed = _baseSpeed;
+                _acceleration = _baseAcceleration;
+            }
+
+            
+            //_horizontalInput and _verticalInput move with linear acceleration in the direction of the camera
+            if (_horizontalInput != 0 || _verticalInput != 0 || _upDownInput != 0)
+            {
+                //increase speed in direction of camera with acceleration
+                _speed += (_transform.forward * _verticalInput).normalized * _acceleration * timeCorrection;
+                _speed += (_transform.right * _horizontalInput).normalized * _acceleration * timeCorrection;
+                _speed += (_transform.up * _upDownInput).normalized * _acceleration * timeCorrection;
+                /* //extra drift while moving not working
+                Vector3 deltaSpeed = _speed.normalized * _acceleration;
+                if(_verticalInput == 0){
+                    if(_speed.magnitude > deltaSpeed.magnitude)
+                        _speed -= Mathf.Sign(Vector3.Dot(_speed, _transform.forward)) * (_transform.forward) * _acceleration * timeCorrection;
+                }
+                if(_horizontalInput == 0){
+                    if(_speed.magnitude > deltaSpeed.magnitude)
+                        _speed -= Mathf.Sign(Vector3.Dot(_speed, _transform.right)) * (_transform.right) * _acceleration * timeCorrection;
+                }
+                if(_upDownInput == 0){
+                    if(_speed.magnitude > deltaSpeed.magnitude)
+                        _speed -= Mathf.Sign(Vector3.Dot(_speed, _transform.up)) * (_transform.up) * _acceleration * timeCorrection;
+                }
+                */
+                //limit speed
+                _speed = _speed.normalized * Mathf.Min(_speed.magnitude, _maxSpeed);
+            }
+            else
+            {
+                Vector3 deltaSpeed = _speed.normalized * _acceleration;
+                if(_speed.magnitude > deltaSpeed.magnitude && _speed.magnitude > _maxSpeed * _smoothBrakeRange)
+                {
+                    _speed -= deltaSpeed * timeCorrection;
+                    _smoothBrake = false;
+                }
+                else
+                {
+                    _smoothBrake = true;
+                    if(_smoothBrake){
+                        //speed *= power 1/timecorrection of smoothbrake
+                        _speed *= Mathf.Pow(_smoothBrakeFactor, timeCorrection);
+                    }
+                    //_speed = Vector3.zero;
+                }
+            }
+            //move in direction of speed
+            transform.position += _speed * timeCorrection;
+        }
+        else
+        {
+            _speed = Vector3.zero;
+        }
+    }
+
+    private bool _smoothBrake = false;
 
     private void OnTriggerEnter(Collider other)
     {
