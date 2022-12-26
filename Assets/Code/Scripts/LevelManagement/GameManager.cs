8c21b49bd321bcba2c9604d21d1dca6f328ea419
bottolo <64470114+bottolo@users.noreply.github.com>
2022-12-26 21:43:45 +0100

    Better style

--- Assets/Code/Scripts/LevelManagement/GameManager.cs
+++ Assets/Code/Scripts/LevelManagement/GameManager.cs
@@ -127,4 +127,5 @@ public class GameManager : MonoBehaviour
     {
         return _gravity;
     }
+    
 }
--- Assets/Code/Scripts/LoadingManagement/LoadingScreenController.cs
+++ Assets/Code/Scripts/LoadingManagement/LoadingScreenController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.Rendering;
 using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,6 +13,7 @@ public class LoadingScreenController : MonoBehaviour
     [SerializeField] private LoadingSceneOption _loadingSceneOption;
     [SerializeField] private TMP_Text loadingText = null;
     [SerializeField] private TMP_Text loadingDots = null;
+    [SerializeField] private GameObject DialogueBox;
     [SerializeField] private GameObject pressButtons;
     [SerializeField] private Image loadingCircle;
     float _firstTime = 0;
@@ -25,7 +27,11 @@ public class LoadingScreenController : MonoBehaviour
     {
         loadingCircle.fillAmount = 0;
         _multiplier = Random.Range(3f, 5f);
+        
         string[] dialogs = _loadingSceneOption._dialogs;
+
+        StartCoroutine(FadeInAndOutCoroutine(DialogueBox, true, 3f));
+
         loadingText.text = dialogs[_loadingSceneOption.GetSceneNumber()];
 
     }
@@ -54,4 +60,105 @@ public class LoadingScreenController : MonoBehaviour
             }
         }
     }
+    
+        private IEnumerator FadeInAndOutCoroutine(GameObject objectToFade, bool fadeIn, float duration)
+            {
+                float counter = 0f;
+
+                //Set Values depending on if fadeIn or fadeOut
+                float a, b;
+                if (fadeIn)
+                {
+                    a = 0;
+                    b = 1;
+                }
+                else
+                {
+                    a = 1;
+                    b = 0;
+                }
+
+                int mode = 0;
+                Color currentColor = Color.clear;
+
+                SpriteRenderer tempSPRenderer = objectToFade.GetComponent<SpriteRenderer>();
+                Image tempImage = objectToFade.GetComponent<Image>();
+                RawImage tempRawImage = objectToFade.GetComponent<RawImage>();
+                MeshRenderer tempRenderer = objectToFade.GetComponent<MeshRenderer>();
+                Text tempText = objectToFade.GetComponent<Text>();
+
+                //Check if this is a Sprite
+                if (tempSPRenderer != null)
+                {
+                    currentColor = tempSPRenderer.color;
+                    mode = 0;
+                }
+                //Check if Image
+                else if (tempImage != null)
+                {
+                    currentColor = tempImage.color;
+                    mode = 1;
+                }
+                //Check if RawImage
+                else if (tempRawImage != null)
+                {
+                    currentColor = tempRawImage.color;
+                    mode = 2;
+                }
+                //Check if Text 
+                else if (tempText != null)
+                {
+                    currentColor = tempText.color;
+                    mode = 3;
+                }
+
+                //Check if 3D Object
+                else if (tempRenderer != null)
+                {
+                    currentColor = tempRenderer.material.color;
+                    mode = 4;
+
+                    //ENABLE FADE Mode on the material if not done already
+                    tempRenderer.material.SetFloat("_Mode", 2);
+                    tempRenderer.material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                    tempRenderer.material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                    tempRenderer.material.SetInt("_ZWrite", 0);
+                    tempRenderer.material.DisableKeyword("_ALPHATEST_ON");
+                    tempRenderer.material.EnableKeyword("_ALPHABLEND_ON");
+                    tempRenderer.material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                    tempRenderer.material.renderQueue = 3000;
+                }
+                else
+                {
+                    yield break;
+                }
+
+                while (counter < duration)
+                {
+                    counter += Time.deltaTime;
+                    float alpha = Mathf.Lerp(a, b, counter / duration);
+
+                    switch (mode)
+                    {
+                        case 0:
+                            tempSPRenderer.color = new Color(currentColor.r, currentColor.g, currentColor.b, alpha);
+                            break;
+                        case 1:
+                            tempImage.color = new Color(currentColor.r, currentColor.g, currentColor.b, alpha);
+                            break;
+                        case 2:
+                            tempRawImage.color = new Color(currentColor.r, currentColor.g, currentColor.b, alpha);
+                            break;
+                        case 3:
+                            tempText.color = new Color(currentColor.r, currentColor.g, currentColor.b, alpha);
+                            break;
+                        case 4:
+                            tempRenderer.material.color = new Color(currentColor.r, currentColor.g, currentColor.b, alpha);
+                            break;
+                    }
+                    
+                    yield return null;
+                }
+            }
+        
 }
