5623bf8436a4e30e44bb488908d1fcdde0165c47
Stefano Fossati <79704488+stefanofossati@users.noreply.github.com>
2023-01-01 20:57:11 +0100

    Add blinking crossair (#225)
    Now when the time is runnign out of time the crossair start to blink in
    two different mode that can be selected:
    - white-trasparent blinking
    - white-black blinking
    
    This selection can be done in the GUI scene
    
    Co-authored-by: stefanofossati <10569836@mail.polimi.it>
    Co-authored-by: spig95 <nicolaischia95@gmail.com>

--- Assets/Code/Scripts/GuiManagement/TimeLeftInPuzzleController.cs
+++ Assets/Code/Scripts/GuiManagement/TimeLeftInPuzzleController.cs
@@ -1,11 +1,7 @@
 using System;
 using System.Collections;
-using System.Net.Mime;
 using POLIMIGameCollective;
-using TMPro;
-using Unity.Mathematics;
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.UI;
 
 namespace Code.Scripts.GuiManagement
@@ -16,77 +12,33 @@ namespace Code.Scripts.GuiManagement
         [SerializeField] private Image _timerImage;
         [SerializeField] private AudioSource _timerAudio;
         [SerializeField] private float _clockTickThreshold = 0.33f;
+        [SerializeField] private float _blinkingTimeFrequency = 0.5f;
+        [SerializeField] private bool _blinkWhiteBlack = false;
         
         private bool _isActive;
         private bool _isRunning;
         private float _timeLeftInPuzzle;
         private float _totalTimeForPuzzle;
         private bool _isClockActive;
+        
+        private Color _crossairOriginalColor;
+        private Coroutine _blinkingCrossairCoroutine;
+        private bool _blinkingCoroutineIsRunning;
+
+        #region Unity Methods
 
         private void Awake()
         {
             //_timerText.text = "";
             _timerImage.fillAmount = 0f;
             _isClockActive = false;
+            _crossairOriginalColor = _timerImage.color;
             
             EventManager.StartListening("GuiResetTimer", ResetTimer);
             EventManager.StartListening("GuiResumeTimer", ResumeTimer);
-            EventManager.StartListening("GuiPauseTimer", PauseTimer);
-        }
-
-        private void ResumeTimer()
-        {
-            EventManager.StopListening("GuiResumeTimer", ResumeTimer);
-            _isActive = true;
-            _isRunning = true;
-            
-            if (_isClockActive) 
-                _timerAudio.Play();
-            
-            Debug.Log("Started timer"+ _timeLeftInPuzzle);
-            EventManager.StartListening("GuiResumeTimer", ResumeTimer);
-        }
-
-        private void PauseTimer()
-        {
-            EventManager.StopListening("GuiPauseTimer", PauseTimer);
-            _isRunning = false;
-            _timerAudio.Pause();
-            Debug.Log("Timer paused!" + _timeLeftInPuzzle);
             EventManager.StartListening("GuiPauseTimer", PauseTimer);
         }
         
-        private void ResetTimer(string totalTimeForPuzzleStr)
-        {
-            EventManager.StopListening("GuiResetTimer", ResetTimer);
-            var totalTimeForPuzzle = float.Parse(totalTimeForPuzzleStr);
-            
-            _isRunning = false;
-            _isClockActive = false;
-            _timerAudio.Stop();
-            
-            if (totalTimeForPuzzle == 0)
-            {
-                Debug.Log("Reset timer: no time limit for this puzzle!");
-                //_timerText.text = "NO TIME ZONE";
-                _timerImage.fillAmount = 0;
-                _isActive = false;
-            }
-            else if (totalTimeForPuzzle > 0)
-            {
-                Debug.Log($"Reset timer: time limit set {totalTimeForPuzzle} for this puzzle!");
-                _isActive = true;
-                _timeLeftInPuzzle = totalTimeForPuzzle;
-                _totalTimeForPuzzle = totalTimeForPuzzle;
-                UpdateRenderValues();
-            }
-            else
-            {
-                throw new Exception("The timer must have a value bigger than 0! Got + " + totalTimeForPuzzle);
-            }
-            EventManager.StartListening("GuiResetTimer", ResetTimer);
-        }
-
         private void Update()
         {
             if (!_isActive)
@@ -99,16 +51,218 @@ namespace Code.Scripts.GuiManagement
             }
         }
 
+        #endregion
+
+        #region private Methods
+
+        private void ResumeTimer()
+        {
+            EventManager.StopListening("GuiResumeTimer", ResumeTimer);
+            _isActive = true;
+            _isRunning = true;
+
+            if (_isClockActive)
+            {
+                _timerAudio.Play();
+                if (!_blinkingCoroutineIsRunning)
+                    if (_blinkWhiteBlack)
+                    {
+                        _blinkingCrossairCoroutine = StartCoroutine(BlinkingCrossairWhiteBlackCoroutine());
+                    }
+                    else
+                    {
+                        _blinkingCrossairCoroutine = StartCoroutine(BlinkingCrossairColorTrasparentCoroutine());
+                    }
+            }
+            
+            // Debug.Log("Started timer" + _timeLeftInPuzzle);
+            EventManager.StartListening("GuiResumeTimer", ResumeTimer);
+        }
+
+        private void PauseTimer()
+        {
+            EventManager.StopListening("GuiPauseTimer", PauseTimer);
+            _isRunning = false;
+            _timerAudio.Pause();
+            StopBlinkingCoroutine();
+            // Debug.Log("Timer paused!" + _timeLeftInPuzzle);
+            EventManager.StartListening("GuiPauseTimer", PauseTimer);
+        }
+        
+        private void ResetTimer(string totalTimeForPuzzleStr)
+        {
+            EventManager.StopListening("GuiResetTimer", ResetTimer);
+            var totalTimeForPuzzle = float.Parse(totalTimeForPuzzleStr);
+            
+            _isRunning = false;
+            _isClockActive = false;
+            _timerAudio.Stop();
+            StopBlinkingCoroutine();
+            
+            if (totalTimeForPuzzle == 0)
+            {
+                // Debug.Log("Reset timer: no time limit for this puzzle!");
+                _timerImage.fillAmount = 0;
+                _isActive = false;
+            }
+            else if (totalTimeForPuzzle > 0)
+            {
+                // Debug.Log($"Reset timer: time limit set {totalTimeForPuzzle} for this puzzle!");
+                _isActive = true;
+                _timeLeftInPuzzle = totalTimeForPuzzle;
+                _totalTimeForPuzzle = totalTimeForPuzzle;
+                UpdateRenderValues();
+            }
+            else
+            {
+                throw new Exception("The timer must have a value bigger than 0! Got + " + totalTimeForPuzzle);
+            }
+            EventManager.StartListening("GuiResetTimer", ResetTimer);
+        }
+
         private void UpdateRenderValues()
         {
-            //_timerText.text = Mathf.RoundToInt(_timeLeftInPuzzle).ToString();
             _timerImage.fillAmount = _timeLeftInPuzzle / _totalTimeForPuzzle;
 
             if ((_timeLeftInPuzzle / _totalTimeForPuzzle) <= _clockTickThreshold && !_isClockActive)
             { 
                 _timerAudio.Play();
+                if (_blinkWhiteBlack)
+                {
+                    _blinkingCrossairCoroutine = StartCoroutine(BlinkingCrossairWhiteBlackCoroutine());
+                }
+                else
+                {
+                    _blinkingCrossairCoroutine = StartCoroutine(BlinkingCrossairColorTrasparentCoroutine());
+                }
+                
                 _isClockActive = true;
+                
             }
         }
+        
+        private void StopBlinkingCoroutine()
+        {
+            if (_blinkingCoroutineIsRunning)
+            {
+                StopCoroutine(_blinkingCrossairCoroutine);
+                _timerImage.color = _crossairOriginalColor;
+            }
+        }
+
+        #endregion
+        
+
+        #region Coroutines
+
+        private IEnumerator BlinkingCrossairColorTrasparentCoroutine()
+        {
+            _blinkingCoroutineIsRunning = true;
+            //Debug.Log("startcouroutine blink");
+            float elapsedTime;
+
+            Color crossairColor = _timerImage.color;
+            float blinkingTime = _blinkingTimeFrequency;
+            //Debug.Log(crossairColor);
+
+            while (_timeLeftInPuzzle > 0)
+            {
+                elapsedTime = 0f;
+                while (elapsedTime < blinkingTime)
+                {
+                    // Debug.Log("blink out");
+                    elapsedTime += Time.deltaTime;
+
+                    float alpha = Mathf.Lerp(crossairColor.a, 0, elapsedTime / (blinkingTime));
+
+                    _timerImage.color = new Color(crossairColor.r, crossairColor.g, crossairColor.b, alpha); 
+                    
+                    // Debug.Log(_timerImage.color);
+                    if (alpha <= 0)
+                    {
+                        // Debug.Log("Crossair trasparent: " + _timerImage.color.a);
+                    }
+
+                    yield return new WaitForEndOfFrame();
+                }
+            
+                
+                elapsedTime = 0f;
+                while (elapsedTime < blinkingTime)
+                {
+                    // Debug.Log("blink in");
+                    elapsedTime += Time.deltaTime;
+                
+                    float alpha = Mathf.Lerp(0, crossairColor.a, elapsedTime / (blinkingTime)); 
+
+                    _timerImage.color = new Color(crossairColor.r, crossairColor.g, crossairColor.b, alpha);
+                    // Debug.Log(_timerImage.color);
+
+                    if (alpha >= crossairColor.a)
+                    {
+                        // Debug.Log("Crossair complete: " + _timerImage.color.a);
+                    }
+                    yield return new WaitForEndOfFrame();
+                }
+            }
+            
+            //Debug.Log("Finish coroutine");
+            _blinkingCoroutineIsRunning = false;
+            _timerImage.color = _crossairOriginalColor;
+            yield return null;
+        }
+
+        private IEnumerator BlinkingCrossairWhiteBlackCoroutine()
+        {
+            _blinkingCoroutineIsRunning = true;
+            //Debug.Log("startcouroutine blink");
+            float elapsedTime;
+
+            Color crossairColor = _timerImage.color;
+            float blinkingTime = _blinkingTimeFrequency;
+            // Debug.Log(crossairColor);
+
+            while (_timeLeftInPuzzle > 0)
+            {
+                elapsedTime = 0f;
+                while (elapsedTime < blinkingTime)
+                {
+                    // Debug.Log("blink out");
+                    elapsedTime += Time.deltaTime;
+
+                    float red = Mathf.Lerp(crossairColor.r, 0, elapsedTime / blinkingTime);
+                    float blue = Mathf.Lerp(crossairColor.b, 0, elapsedTime / blinkingTime);
+                    float green = Mathf.Lerp(crossairColor.g, 0, elapsedTime / blinkingTime);
+                    
+                    _timerImage.color = new Color(red, blue, green, crossairColor.a);
+
+                    yield return new WaitForEndOfFrame();
+                }
+            
+                
+                elapsedTime = 0f;
+                while (elapsedTime < blinkingTime)
+                {
+                    // Debug.Log("blink in");
+                    elapsedTime += Time.deltaTime;
+                
+                    
+                    float red = Mathf.Lerp(0,crossairColor.r, elapsedTime / blinkingTime);
+                    float blue = Mathf.Lerp(0,crossairColor.b, elapsedTime / blinkingTime);
+                    float green = Mathf.Lerp(0,crossairColor.g,  elapsedTime / blinkingTime);
+
+                    _timerImage.color = new Color(red, blue, green, crossairColor.a);
+                    yield return new WaitForEndOfFrame();
+                }
+            }
+            
+            //Debug.Log("Finish coroutine");
+            _blinkingCoroutineIsRunning = false;
+            _timerImage.color = _crossairOriginalColor;
+            yield return null;
+        }
+
+        #endregion
+        
     }
 }
