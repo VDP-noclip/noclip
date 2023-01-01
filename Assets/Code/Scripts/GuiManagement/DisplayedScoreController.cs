fd5dafa5423293e1074b7f33f10fc6463d48c573
spig95 <nicolaischia95@gmail.com>
2023-01-01 18:49:24 +0100

    Add proposal for score implementation

---
+++ Assets/Code/Scripts/GuiManagement/DisplayedScoreController.cs
@@ -0,0 +1,45 @@
+﻿using Code.Scripts.Score;
+using POLIMIGameCollective;
+using TMPro;
+using UnityEngine;
+
+namespace Code.Scripts.GuiManagement
+{
+    /// <summary>
+    /// This is a simple example of how the ScoreManager can be used. This class reacts to the 'RequestGuiUpdateScore',
+    /// an event triggered by the ScoreManager everytime there is a score update to be displayed.
+    /// </summary>
+    public class DisplayedScoreController: MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _scoreText;
+        [SerializeField] private TMP_Text _scoreInfo;
+        
+        private void Start()
+        {
+            EventManager.StartListening("RequestGuiUpdateScore", RequestGuiUpdateScore);
+            RequestGuiUpdateScore();
+        }
+    
+        private void RequestGuiUpdateScore()
+        {
+            if (_scoreText != null)
+            {
+                float currentScore = ScoreManager.GetTotalScore();
+                _scoreText.text = $"Score: {Mathf.Round(currentScore)} \n";
+            }
+            
+            if (_scoreInfo != null)
+            {
+                float[] puzzleStats = ScoreManager.GetStats();
+                float? bestscore = ScoreManager.GetBestScore();
+                _scoreInfo.text = $"N. of deaths: {puzzleStats[0]} \n" +
+                                  $"N. ran out of time: {puzzleStats[1]} \n" +
+                                  $"N. of noclips: {puzzleStats[2]} \n" +
+                                  $"N. of skipped puzzles: {puzzleStats[3]} \n" +
+                                  $"N. of completed puzzles: {puzzleStats[3]} \n" +
+                                  $"Best score: {bestscore}";
+            }
+        }
+    }
+}
+
