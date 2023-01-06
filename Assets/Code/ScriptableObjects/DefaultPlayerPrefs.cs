c7d04fe478577cb5af7d11ce15e15104017bb9ed
spig95 <nicolaischia95@gmail.com>
2023-01-06 12:28:55 +0100

    Set masterSensitivity to 3

--- Assets/Code/ScriptableObjects/DefaultPlayerPrefs.cs
+++ Assets/Code/ScriptableObjects/DefaultPlayerPrefs.cs
@@ -11,6 +11,6 @@ namespace Code.ScriptableObjects
         public float soundTracksVolumeDecibel = 0.9f;
         [Range(0, 2)] public int masterQuality = 1;
         public bool masterFullScreen = true;
-        [Range(0, 7)] public float masterSensitivity;
+        [Range(0, 7)] public float masterSensitivity = 3;
     }
 }
