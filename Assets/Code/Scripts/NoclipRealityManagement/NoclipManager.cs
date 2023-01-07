430d4fc33d62c72b94c35c743869071d2d06db5f
Alessio De Nicolò <115568998+AleDeNic@users.noreply.github.com>
2023-01-08 00:27:30 +0100

    BOOOH ENTRO LE 00:00 (#285)
    HHHHHHH
    
    Co-authored-by: TizioMaurizio <maurizio.vetere@mail.polimi.it>

--- Assets/Code/Scripts/NoclipRealityManagement/NoclipManager.cs
+++ Assets/Code/Scripts/NoclipRealityManagement/NoclipManager.cs
@@ -179,6 +179,8 @@ public class NoclipManager : MonoBehaviour
     {
         EventManager.TriggerEvent("StartNoclipAnimation");
         
+        SetAnimationSpeed("normal");
+
         _postprocessNoclip.SetActive(false);
         _postprocessReality.SetActive(true);
         
