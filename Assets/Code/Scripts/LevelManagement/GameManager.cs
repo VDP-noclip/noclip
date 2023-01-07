bf78a84a68b034cc906a7f66734ac8bf143325ba
TizioMaurizio <maurizio.vetere@mail.polimi.it>
2023-01-07 21:16:24 +0100

    fix double scenes

--- Assets/Code/Scripts/LevelManagement/GameManager.cs
+++ Assets/Code/Scripts/LevelManagement/GameManager.cs
@@ -73,6 +73,7 @@ public class GameManager : MonoBehaviour
                 else
                 {
                     _gameState = GameState.NewArea;
+                    SceneManager.SetActiveScene(gameObject.scene);
                     CloseAllScenes();
                     //DestroyOtherGameObjects();
                     _currentGameAreaIndex += 1;
