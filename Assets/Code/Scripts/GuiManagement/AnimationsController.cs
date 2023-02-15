b5cfa132d9e9e54f906418d9b1bd43bf31e9b779
stefanofossati <10569836@mail.polimi.it>
2023-02-15 13:09:18 +0100

    add credits

--- Assets/Code/Scripts/GuiManagement/AnimationsController.cs
+++ Assets/Code/Scripts/GuiManagement/AnimationsController.cs
@@ -19,6 +19,7 @@ public class AnimationsController : MonoBehaviour
     [SerializeField] private GameObject feedbackButton;
     [SerializeField] private GameObject controlsButton;
     [SerializeField] private GameObject statisticsButton;
+    [SerializeField] private GameObject creditsButton;
     
     [Header("Audio To Play")] 
     [SerializeField] private AudioSource noclipEcho;
@@ -45,6 +46,8 @@ public class AnimationsController : MonoBehaviour
                 StartCoroutine(FadeInAndOutCoroutine(controlsButton, true, 0.1f));
                 yield return new WaitForSecondsRealtime(0.2f);
                 StartCoroutine(FadeInAndOutCoroutine(statisticsButton, true, 0.1f));
+                yield return new WaitForSecondsRealtime(0.2f);
+                StartCoroutine(FadeInAndOutCoroutine(creditsButton, true, 0.1f));
                 yield return new WaitForSecondsRealtime(1f);
                 StartCoroutine(FadeInAndOutCoroutine(logoBlur, true, 0.1f));
                 noclipEcho.Play();
@@ -70,6 +73,8 @@ public class AnimationsController : MonoBehaviour
             StartCoroutine(FadeInAndOutCoroutine(controlsButton, false, 0.5f));
             yield return new WaitForSecondsRealtime(0.5f);
             StartCoroutine(FadeInAndOutCoroutine(statisticsButton, false, 0.5f));
+            yield return new WaitForSecondsRealtime(0.5f);
+            StartCoroutine(FadeInAndOutCoroutine(creditsButton, false, 0.5f));
             yield return new WaitForSecondsRealtime(1f);
             StartCoroutine(FadeInAndOutCoroutine(logoBlur, false, 0.5f));
             noclipEcho.Play();
